04229705c90813ee9c94aba767f9fe1deb848dfe
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-13 18:45:21 +0900

    ✨feat : useUserLocationStore 연결

---
+++ src/app/log/record/LogRecord.constants.ts
@@ -0,0 +1,16 @@
+import { LogType } from "@/types/Request/Masils";
+
+export const DEFAULT_LOG_DATA: LogType = {
+  location1: "",
+  location2: "",
+  location3: "",
+  path: [],
+  title: "",
+  content: "",
+  distance: 0,
+  totalTime: 0,
+  startedAt: "",
+  pinPoints: [],
+  thumbnail: null,
+  postId: "",
+};
--- src/app/log/record/LogRecord.model.ts
+++ src/app/log/record/LogRecord.model.ts
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { LogPageStep } from "./LogRecord.types";
 import { LogType, Position } from "@/types/Request/Masils";
+import useUserLocationStore from "@/stores/useUserLocationStore";
 
 const DEFAULT_LOG_DATA: LogType = {
   location1: "",
@@ -18,13 +19,13 @@ const DEFAULT_LOG_DATA: LogType = {
 };
 
 const useLogRecordModel = () => {
-  const [pageStep, setPageStep] = useState<LogPageStep>("LOG_RECORD_STANDBY");
+  const { userLocation } = useUserLocationStore();
 
+  const [pageStep, setPageStep] = useState<LogPageStep>("LOG_RECORD_STANDBY");
   const [logData, setLogData] = useState<LogType>(DEFAULT_LOG_DATA);
 
   // 이후 Store 에서 가져와야함
-  const [center, setCentner] = useState<Position>({ lat: 0, lng: 0 });
-
+  const [center, setCentner] = useState<Position>(userLocation);
   const [watchCode, setWatchCode] = useState(0);
 
   return {
