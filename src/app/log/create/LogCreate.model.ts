7908f17ee4bcdd013ae1540e1e53843a597c22ef
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-13 15:30:14 +0900

    ✨feat : 기본 LogCreate Model 구현

--- src/app/log/create/LogCreate.model.ts
+++ src/app/log/create/LogCreate.model.ts
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { LogCreateStep, LogType, Position } from "./LogCreate.types";
+
+const DEFAULT_LOG_DATA: LogType = {
+  location1: "",
+  location2: "",
+  location3: "",
+  path: [],
+  title: "",
+  content: "",
+  distance: 0,
+  totalTime: 0,
+  startedAt: "",
+  pinPoints: [],
+  thumbnail: null,
+  postId: "",
+};
+
 const useLogCreateModel = () => {
-  return {};
+  const [createStep, setCreateStep] = useState<LogCreateStep>("LOG_CREATE_STEP_1");
+  const [logData, setLogData] = useState<LogType>(DEFAULT_LOG_DATA);
+
+  // 이후 Store 에서 가져와야함
+  const [center, setCentner] = useState<Position>({ lat: 0, lng: 0 });
+  const [watchCode, setWatchCode] = useState(0);
+
+  return {
+    createStep,
+    setCreateStep,
+    logData,
+    setLogData,
+    center,
+    setCentner,
+    watchCode,
+    setWatchCode,
+  };
 };
 
 export default useLogCreateModel;
