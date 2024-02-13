e9b958119ca614e082f83ca73db0857db19e7d95
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-14 00:48:52 +0900

    ✨feat : useEffect cleanUp 동작 테스트

--- src/app/log/record/components/LogRecordEdit/LogRecordEdit.controller.tsx
+++ src/app/log/record/components/LogRecordEdit/LogRecordEdit.controller.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import LogRecordEditView from "./LogRecordEdit.view";
 
 const LogRecordEditController = () => {
+  useEffect(() => {
+    // clean up
+    return () => {
+      console.log("Edit Component CleanUp");
+    };
+  }, []);
   return <LogRecordEditView />;
 };
 
--- src/app/log/record/components/LogRecordRecording/LogRecordRecording.controller.tsx
+++ src/app/log/record/components/LogRecordRecording/LogRecordRecording.controller.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import LogRecordRecordingView from "./LogRecordRecording.view";
 
 const LogRecordRecordingController = () => {
+  useEffect(() => {
+    // clean up
+    return () => {
+      console.log("Recording Component cleanUp");
+    };
+  }, []);
+
   return <LogRecordRecordingView />;
 };
 
