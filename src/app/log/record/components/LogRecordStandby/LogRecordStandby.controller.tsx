5f8fdeca20774a4fca56e9c823df83adae281f49
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-14 00:49:33 +0900

    ✨feat : WatchPosition 정상 동작 테스트

--- src/app/log/record/components/LogRecordStandby/LogRecordStandby.controller.tsx
+++ src/app/log/record/components/LogRecordStandby/LogRecordStandby.controller.tsx
@@ -1,7 +1,38 @@
+import { useEffect } from "react";
 import LogRecordStandbyView from "./LogRecordStandby.view";
 
-const LogRecordStandbyController = () => {
-  return <LogRecordStandbyView />;
+interface LogRecordStandbyControllerProps {
+  watchCode: number;
+  setWatchCode: (code: number) => void;
+  onChangeCenter: (coords: { lat: number; lng: number }) => void;
+}
+
+const LogRecordStandbyController = ({
+  watchCode,
+  setWatchCode,
+  onChangeCenter,
+}: LogRecordStandbyControllerProps) => {
+  const successWatch = ({ coords }: GeolocationPosition) => {
+    onChangeCenter({ lat: coords.latitude, lng: coords.longitude });
+  };
+
+  const errorWatch = () => {};
+
+  const handleClick = () => {
+    const newWatchCode = navigator.geolocation.watchPosition(successWatch, errorWatch, {
+      enableHighAccuracy: true,
+    });
+
+    setWatchCode(newWatchCode);
+  };
+
+  useEffect(() => {
+    return () => {
+      navigator.geolocation.clearWatch(watchCode);
+    };
+  }, []);
+
+  return <LogRecordStandbyView onClick={handleClick} />;
 };
 
 export default LogRecordStandbyController;
--- src/app/log/record/components/LogRecordStandby/LogRecordStandby.view.tsx
+++ src/app/log/record/components/LogRecordStandby/LogRecordStandby.view.tsx
@@ -1,5 +1,14 @@
-const LogRecordStandbyView = () => {
-  return <>LogRecordStandby.view</>;
+interface LogRecordStandbyViewProps {
+  onClick: () => void;
+}
+
+const LogRecordStandbyView = ({ onClick }: LogRecordStandbyViewProps) => {
+  return (
+    <>
+      LogRecordStandby.view
+      <button onClick={onClick}> setWatchCode</button>
+    </>
+  );
 };
 
 export default LogRecordStandbyView;
