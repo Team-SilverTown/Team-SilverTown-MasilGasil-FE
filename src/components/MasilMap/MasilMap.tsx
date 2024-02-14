59c4fd0a4bea64b04834778ec5cdc56e3122d4fc
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-14 13:56:58 +0900

    ✨feat: Masil Map 에 대한 JS DOC 추가

--- src/components/MasilMap/MasilMap.tsx
+++ src/components/MasilMap/MasilMap.tsx
@@ -37,6 +37,30 @@ interface MasilMapProps {
   pinSize?: number;
 }
 
+/**
+ * @summary MasilGasil 프로젝트에서 내에서  사용가능한 Map!
+ *
+ * @param center 지도의 center 위치 - type : GeoJSONPoint
+ * @param path 이동 경로의 좌표값들  - type : GeoJSONLineString
+ * @param pins 핀 리스트 - type : Pin
+ *
+ * @param draggable 지도의 드래그 허용 여부 - type : boolean
+ * @param zoomable 지도의 확대 여부 - type : boolean
+ * @param minZoomLevel 지도의 최소 줌 level - type : number
+ * @param maxZoomLevel 지도의 최대 줌 level - type : number
+ *
+ * @param isShowCenterMarker 현재 사용자의 위치에 중앙 마커가 보일지 여부 - type : boolean
+ * @param centerMarkerSize 중앙 마커의 크기 ( 기본값 32px ) - type : number (px)
+ * @param centerMarkerFill 중앙 마커의 색상 - type : string
+ *
+ * @param onCreatePath 이동 경로가 그려질때 마다 발동하는 이벤트 함수 ( 거리 계산에 이용 ) - type : (polyline: kakao.maps.Polyline) => void;
+ * @param pathColor 이동 경로의 색상 - type : string
+ * @param pathOpacity 이동 경로의 투명도 - type : 0 ~ 1
+ * @param pathWeight 이동 경로의 두께 - type : 1 2 3 4 5 6 7 8 9 10
+ *
+ * @param onClickPin 핀 클릭 이벤트 - type : 미정
+ * @param pinSize 핀의 사이즈 ( 기본값 24px ) - type : number ( px )
+ */
 const MasilMap = ({
   center,
   path,
