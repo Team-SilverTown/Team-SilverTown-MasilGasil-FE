b7480f81f6b53b1cf95ebd4331f7db7ffaaf63c5
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-15 11:32:17 +0900

    ✨feat: watch 구현

--- src/app/log/record/components/LogRecordStandby/LogRecordStandby.controller.tsx
+++ src/app/log/record/components/LogRecordStandby/LogRecordStandby.controller.tsx
@@ -1,5 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import LogRecordStandbyView from "./LogRecordStandby.view";
+import useUserLocationStore from "@/stores/useUserLocationStore";
+
+import throttle from "lodash.throttle";
 
 interface LogRecordStandbyControllerProps {
   watchCode: number;
@@ -10,7 +13,26 @@ const LogRecordStandbyController = ({
   watchCode,
   setWatchCode,
 }: LogRecordStandbyControllerProps) => {
+  const { setUserLocation } = useUserLocationStore();
+
+  /**
+   * @summary Watcher 가 동작한 후 정상적으로 위치를 가져왔을대 실행되는 함수입니다.
+   *
+   * ( throttle을 이용해 setter를 갱신시키기 위해선 useRef를 사용해야합니다. )
+   */
+  const successWatch = useRef(
+    throttle(({ coords }: GeolocationPosition) => {
+      const { latitude, longitude } = coords;
+      setUserLocation({ lat: latitude, lng: longitude });
+
+      console.log("동작합니다");
+    }, 200),
+  ).current;
+
   useEffect(() => {
+    const newWatchCode = navigator.geolocation.watchPosition(successWatch);
+    setWatchCode(newWatchCode);
+
     return () => {
       navigator.geolocation.clearWatch(watchCode);
       console.log("Standby Component cleanUp");
