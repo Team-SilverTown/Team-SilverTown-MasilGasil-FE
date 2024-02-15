65b456b3b2bf0d6c1314f00cbc4f3ba1597d50a7
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-15 22:24:19 +0900

    ✨feat: Recording Step 스타일

---
+++ src/app/log/record/components/LogRecordRecording/LogRecordRecording.styles.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const LogRecordActionLayout = styled.div`
+  position: absolute;
+  bottom: 0;
+
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+`;
+
+export const LogRecordActionContainer = styled.div`
+  width: 32rem;
+  padding: 1rem;
+  margin-bottom: 1.6rem;
+
+  /* 추후 전역 스타일 적용 */
+  background-color: ${({ theme }) => "#fff"};
+  border-radius: 0.8rem;
+  box-shadow: 0 2px 7.8px 0 ${(props) => props.theme.transparent_50};
+`;
+
+export const LogRecordInfoContainer = styled.div`
+  width: 30rem;
+  margin-bottom: 1rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const LogRecordInfo = styled.p`
+  width: 14rem;
+  height: 4rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  /* 추후 전역 스타일 적용 */
+  background-color: #f1f5eb;
+  border-radius: 0.8rem;
+  box-shadow: 0 2px 7.8px 0 ${(props) => props.theme.transparent_50};
+
+  /* 추후 전역 스타일 적용 */
+  font-size: 1.8rem;
+  font-weight: 700;
+`;
--- src/app/log/record/components/LogRecordRecording/LogRecordRecording.view.tsx
+++ src/app/log/record/components/LogRecordRecording/LogRecordRecording.view.tsx
@@ -1,5 +1,36 @@
+import { Button } from "@/components";
+import * as S from "./LogRecordRecording.styles";
+import Theme, { BUTTON_FULL_WIDTH, FONT_WEIGHT } from "@/styles/theme";
+import { MapPin } from "@/components/icons";
+
 const LogRecordRecordingView = () => {
-  return <>LogRecordRecording.view</>;
+  return (
+    <S.LogRecordActionLayout>
+      <Button
+        style={{ marginBottom: "1rem" }}
+        variant="neumorp"
+      >
+        <MapPin fill={Theme.lightTheme.yellow_500} />
+      </Button>
+
+      <S.LogRecordActionContainer>
+        <S.LogRecordInfoContainer>
+          <S.LogRecordInfo></S.LogRecordInfo>
+          <S.LogRecordInfo></S.LogRecordInfo>
+        </S.LogRecordInfoContainer>
+
+        <Button
+          buttonColor={Theme.lightTheme.green_500}
+          variant="neumorp"
+          textColor={Theme.lightTheme.white}
+          style={{ fontWeight: FONT_WEIGHT.BOLD, opacity: 0.8 }}
+          width={BUTTON_FULL_WIDTH}
+        >
+          마실 기록 시작
+        </Button>
+      </S.LogRecordActionContainer>
+    </S.LogRecordActionLayout>
+  );
 };
 
 export default LogRecordRecordingView;
