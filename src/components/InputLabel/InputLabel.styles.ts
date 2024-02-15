9d6da9abcce3787d4495d54f44d60f5b91140366
Zoody <reopard506@gmail.com>
2024-02-15 11:54:46 +0900

    🎨style: styled components if -> swith 문으로 변경

--- src/components/InputLabel/InputLabel.styles.ts
+++ src/components/InputLabel/InputLabel.styles.ts
@@ -6,40 +6,42 @@ export const Label = styled.label<{ $position: "start" | "center" | "end" }>`
   width: 100%;
 
   ${({ $position }) => {
-    if ($position === "start") {
-      return css`
-        justify-content: start;
-      `;
-    } else if ($position === "center") {
-      return css`
-        justify-content: center;
-      `;
-    } else {
-      return css`
-        justify-content: end;
-      `;
+    switch ($position) {
+      case "start":
+        return css`
+          justify-content: start;
+        `;
+      case "center":
+        return css`
+          justify-content: center;
+        `;
+      case "end":
+        return css`
+          justify-content: end;
+        `;
     }
-  }};
+  }}
 `;
 
 export const Text = styled.span<{ $type: "normal" | "safety" | "warn" | "danger" }>`
   ${({ $type }) => {
-    if ($type === "safety") {
-      return css`
-        color: ${(props) => props.theme.green_500};
-      `;
-    } else if ($type === "warn") {
-      return css`
-        color: ${(props) => props.theme.yellow_500};
-      `;
-    } else if ($type === "danger") {
-      return css`
-        color: ${(props) => props.theme.red_500};
-      `;
-    } else {
-      return css`
-        color: ${(props) => props.theme.text_primary_color + 50};
-      `;
+    switch ($type) {
+      case "danger":
+        return css`
+          color: ${(props) => props.theme.red_500};
+        `;
+      case "warn":
+        return css`
+          color: ${(props) => props.theme.yellow_500};
+        `;
+      case "safety":
+        return css`
+          color: ${(props) => props.theme.green_500};
+        `;
+      default:
+        return css`
+          color: ${(props) => props.theme.text_primary_color + 50};
+        `;
     }
   }};
   font-size: 0.75rem;
