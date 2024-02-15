d841e2008581556a622121ad808dfd3fd4f90400
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-15 12:36:58 +0900

    ✨feat: STEP 1 요소 구현

---
+++ src/app/log/record/components/LogRecordStandby/LogRecordStandby.Styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const LogRecordStandbyLayout = styled.article`
+  width: 100%;
+  max-width: 40rem;
+  padding: 0 1.6rem 1.6rem 1.6rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  position: absolute;
+  bottom: 0;
+`;
+
+export const LogRecordStandbyButton = styled.div`
+  width: 90%;
+  height: 40px;
+`;
--- src/app/log/record/components/LogRecordStandby/LogRecordStandby.view.tsx
+++ src/app/log/record/components/LogRecordStandby/LogRecordStandby.view.tsx
@@ -1,9 +1,24 @@
+import Theme, { FONT_WEIGHT } from "@/styles/theme";
+import * as S from "./LogRecordStandby.Styles";
+import { Button } from "@/components";
+
 interface LogRecordStandbyViewProps {
   onClick: () => void;
 }
 
 const LogRecordStandbyView = ({ onClick }: LogRecordStandbyViewProps) => {
-  return <></>;
+  return (
+    <S.LogRecordStandbyLayout>
+      <Button
+        buttonColor={Theme.lightTheme.green_500}
+        textColor={Theme.lightTheme.white}
+        style={{ fontWeight: FONT_WEIGHT.BOLD }}
+        width={"100%"}
+      >
+        마실 기록 시작
+      </Button>
+    </S.LogRecordStandbyLayout>
+  );
 };
 
 export default LogRecordStandbyView;
