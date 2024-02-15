b2fa62674f677c6a946541da721b1735534b4db4
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-15 13:01:28 +0900

    🎨style: Theme 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,4 @@
-export const lightTheme: object = {
+export const lightTheme = {
   theme_mode: "light",
   background_color: "#FDFAF6",
   container_color: "#FEFEFE",
@@ -6,6 +6,7 @@ export const lightTheme: object = {
   text_secondary_color: "#F0F0F0",
   black_900: "#232323",
   white_900: "#FDFAF6",
+  white: "#FFFFFF",
   gray_500: "#909090",
   gray_300: "#B3B3B3",
   gray_200: "#E3E3E3",
@@ -22,8 +23,9 @@ export const lightTheme: object = {
   green_300: "#B9DB56",
   green_500: "#81BB26",
 };
+export type LightTheme = typeof lightTheme;
 
-export const darkTheme: object = {
+export const darkTheme = {
   theme_mode: "dark",
   background_color: "#FDFAF6",
   container_color: "#FEFEFE",
@@ -47,6 +49,7 @@ export const darkTheme: object = {
   green_300: "#B9DB56",
   green_500: "#81BB26",
 };
+export type DarkTheme = typeof darkTheme;
 
 const Theme = {
   lightTheme,
@@ -74,12 +77,14 @@ export const MOBILEABLE_CONTAINER_VERTICAL_SPACE: string = "h-[100%] sm:h-screen
 // SCREEN ------------------------------------------ //
 export const APP_MAX_WIDTH = 600; // px
 
-export const SCREEN_SIZE_MB = 450; // 450px
-export const SCREEN_SIZE_SM = 640; // 640px
-export const SCREEN_SIZE_MD = 768; // 768px
-export const SCREEN_SIZE_XMD = 940; // 940px
-export const SCREEN_SIZE_LG = 1024; // 1024px
-export const SCREEN_SIZE_XL = 1280; // 1280px
+export const SCREEN_SIZE = {
+  MB: 45, // rem - 45px
+  SM: 64, // rem - 640px
+  MD: 76.8, // rem - 768px
+  XMD: 94, // rem - 940px
+  LG: 102.4, // rem - 1024px
+  XL: 128, // rem - 1280px
+};
 
 export const MOBILE_LIMIT = 768; // 768px
 export const TABLET_LIMIT = 1025; // 655 + 370px
@@ -93,6 +98,7 @@ export const POSTER_SIMPLE_CARD_HEIGHT: number = 150; // 150px
 export const POSTER_SIMPLE_CARD_WIDTH: number = 150; // 150px
 export const POSTER_DETAIL_CARD_HEIGHT: number = 500; // 500px
 export const POSTER_DETAIL_CARD_WIDTH: number = 600; // 600px
+
 // -------------------------------------------------------- CARDS //
 
 // MODAL -------------------------------------------------------- //
@@ -108,3 +114,13 @@ export const MODAL = 888;
 export const MODAL_BACKGROUND = 888;
 export const MODAL_LAYOUT = 999;
 export const SPINNER = 9999;
+
+// font Weight ------------------------------------------ //
+export const FONT_WEIGHT = {
+  MEDIUM: 400,
+  BOLD: 700,
+};
+
+// floating BUTTON ------------------------------------------ //
+
+export const BUTTON_FULL_WIDTH = "100%";
