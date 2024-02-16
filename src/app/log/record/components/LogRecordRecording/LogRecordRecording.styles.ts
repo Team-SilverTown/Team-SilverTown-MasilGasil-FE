ef11520b7fbedd2af15bb25d4a3e090ce7f8ec33
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-16 09:32:57 +0900

    🎨style: 전체적인 레이아웃 사이즈 조정

--- src/app/log/record/components/LogRecordRecording/LogRecordRecording.styles.ts
+++ src/app/log/record/components/LogRecordRecording/LogRecordRecording.styles.ts
@@ -1,21 +1,25 @@
 import styled from "styled-components";
 
 export const LogRecordActionLayout = styled.div`
-  position: absolute;
-  bottom: 0;
+  width: 100%;
+  max-width: 36rem;
+  min-width: 24rem;
+  padding-bottom: 1.6rem;
 
   display: flex;
   flex-direction: column;
   align-items: end;
+
+  position: absolute;
+  bottom: 0;
 
   opacity: 0.9;
   user-select: none;
 `;
 
 export const LogRecordActionContainer = styled.div`
-  width: 32rem;
+  width: 100%;
   padding: 1.6rem;
-  margin-bottom: 1.6rem;
 
   /* 추후 전역 스타일 적용 */
   background-color: ${({ theme }) => "#fff"};
@@ -24,16 +28,16 @@ export const LogRecordActionContainer = styled.div`
 `;
 
 export const LogRecordInfoContainer = styled.div`
-  width: 30rem;
+  width: 100%;
   margin-bottom: 1.6rem;
 
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  gap: 1.6rem;
 `;
 
 export const LogRecordInfo = styled.p`
-  width: 14rem;
+  width: calc(50% - 0.8rem);
   height: 4rem;
 
   display: flex;
--- src/app/log/record/components/LogRecordStandby/LogRecordStandby.Styles.ts
+++ src/app/log/record/components/LogRecordStandby/LogRecordStandby.Styles.ts
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const LogRecordStandbyLayout = styled.article`
   width: 100%;
-  max-width: 40rem;
+  max-width: 36rem;
+  min-width: 24rem;
   padding: 0 1.6rem 1.6rem 1.6rem;
 
   display: flex;
