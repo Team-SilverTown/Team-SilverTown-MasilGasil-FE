1be5703a37a9b985e34169569df60ee2f7bc7e02
seunghyun <tmdgus7820@naver.com>
2024-02-16 12:37:47 +0900

    🚜change: BoardList 더 보기 버튼 (url 경로) 수정

--- src/app/user/[id]/Mypage.constants.ts
+++ src/app/user/[id]/Mypage.constants.ts
@@ -159,19 +159,19 @@ export const POSTS_DUMMY_DATA: PostsListType[] = [
 export const RECORDLIST_DUMMY_DATA: MyRecordListProps[] = [
   {
     title: "내 최근 산책 기록",
-    urlLink: "my_log",
+    urlLink: "/more?keyword=my_log",
     recordList: MASILS_DUMMY_DATA, // 이 값은 추후 API 데이터로 변경
     type: "Masils",
   },
   {
     title: "내가 작성한 산책로",
-    urlLink: "my_post",
+    urlLink: "/more?keyword=my_post",
     recordList: POSTS_DUMMY_DATA,
     type: "Posts",
   },
   {
     title: "내가 좋아하는 산책로",
-    urlLink: "my_like",
+    urlLink: "/more?keyword=my_like",
     recordList: POSTS_DUMMY_DATA,
     type: "Posts",
   },
