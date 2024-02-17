220e59f1c5fc33b6a0894ac5051e6e88314df420
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-17 22:38:24 +0900

    ♻️refactor: AlertModal 개선

--- src/components/modalViews/AlertModal/AlertModal.styles.ts
+++ src/components/modalViews/AlertModal/AlertModal.styles.ts
@@ -2,16 +2,20 @@ import styled from "styled-components";
 import { FONT_SIZE, FONT_WEIGHT } from "@/styles/theme";
 
 export const AlertModalLayout = styled.div`
-  width: 28rem;
+  width: 32rem;
   padding: 0 2rem;
+
+  display: flex;
+  flex-direction: column;
 
   user-select: none;
 `;
 
 export const AlertModalMessage = styled.p`
   width: 100%;
-  padding: 4rem 0;
+  padding: 5rem 0 4rem 0;
 
   font-size: ${FONT_SIZE.LARGE};
   font-weight: ${FONT_WEIGHT.SEMIBOLD};
+  word-break: keep-all;
 `;
--- src/components/modalViews/AlertModal/AlertModal.tsx
+++ src/components/modalViews/AlertModal/AlertModal.tsx
@@ -1,9 +1,13 @@
 import { useUI } from "@/components/uiContext/UiContext";
 import * as S from "./AlertModal.styles";
 import { ModalLayout } from "@/components/Modal";
+import { Button } from "@/components";
+import Theme, { FONT_SIZE, FONT_WEIGHT } from "@/styles/theme";
+import React from "react";
 
 interface AlertModalProps {
-  message: string;
+  message: React.ReactNode;
+  cancelButtonText?: string;
 }
 
 interface ModalProp {
@@ -12,7 +16,7 @@ interface ModalProp {
 
 const AlertModal = ({ props }: ModalProp) => {
   const { closeModal } = useUI();
-  const { message } = props;
+  const { message, cancelButtonText = "닫기" } = props;
 
   if (!message) {
     closeModal();
@@ -23,6 +27,14 @@ const AlertModal = ({ props }: ModalProp) => {
     <ModalLayout>
       <S.AlertModalLayout>
         <S.AlertModalMessage>{message}</S.AlertModalMessage>
+        <Button
+          variant="neumorp"
+          buttonColor={Theme.lightTheme.gray_200}
+          style={{ fontSize: FONT_SIZE.LARGE, fontWeight: FONT_WEIGHT.SEMIBOLD }}
+          onClickHandler={closeModal}
+        >
+          {cancelButtonText}
+        </Button>
       </S.AlertModalLayout>
     </ModalLayout>
   );
