9b34a3105dd80885a0ae6f9ff6e3bd919dd00ee9
Zoody <reopard506@gmail.com>
2024-02-20 08:48:51 +0900

    ✨feat: DotIndicator 구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button";
+export { default as DotIndicator } from "./DotIndicator";
 export { default as Input } from "./Input";
 export { default as InputLabel } from "./InputLabel";
 export { default as SlideButton } from "./SlideButton";
---
+++ src/stories/DotIndicator.stories.tsx
@@ -0,0 +1,114 @@
+import { useAddonState } from "@storybook/manager-api";
+import Carousel from "nuka-carousel";
+
+import { DotIndicator, PinDetailCard } from "@/components";
+
+export default {
+  title: "Component/DotIndicator",
+  component: DotIndicator,
+  argTypes: { slideIdx: { control: { type: "number", min: 0, max: 5 } } },
+  args: {
+    slideIdx: 0,
+  },
+};
+
+export const Default = (args: any) => {
+  const data = [
+    {
+      width: 390,
+      height: 200,
+      borderRadius: 8,
+      title: "산책로 매력 포인트 1",
+      content: "이러쿵 저러쿵 뭔가 이쁜 부분이 있어서 사진이랑 남겨봅니다. 으하하하",
+      thumbnailURL:
+        "https://github.com/SeungHyune/first-script/assets/114329713/7842e910-8956-43f2-86a4-2c953cb4be04",
+      currentPinIndex: 1,
+      totalPinIndex: 5,
+    },
+    {
+      width: 390,
+      height: 200,
+      borderRadius: 8,
+      title: "산책로 매력 포인트 1",
+      content: "이러쿵 저러쿵 뭔가 이쁜 부분이 있어서 사진이랑 남겨봅니다. 으하하하",
+      thumbnailURL:
+        "https://github.com/SeungHyune/first-script/assets/114329713/7842e910-8956-43f2-86a4-2c953cb4be04",
+      currentPinIndex: 1,
+      totalPinIndex: 5,
+    },
+    {
+      width: 390,
+      height: 200,
+      borderRadius: 8,
+      title: "산책로 매력 포인트 1",
+      content: "이러쿵 저러쿵 뭔가 이쁜 부분이 있어서 사진이랑 남겨봅니다. 으하하하",
+      thumbnailURL:
+        "https://github.com/SeungHyune/first-script/assets/114329713/7842e910-8956-43f2-86a4-2c953cb4be04",
+      currentPinIndex: 1,
+      totalPinIndex: 5,
+    },
+    {
+      width: 390,
+      height: 200,
+      borderRadius: 8,
+      title: "산책로 매력 포인트 1",
+      content: "이러쿵 저러쿵 뭔가 이쁜 부분이 있어서 사진이랑 남겨봅니다. 으하하하",
+      thumbnailURL:
+        "https://github.com/SeungHyune/first-script/assets/114329713/7842e910-8956-43f2-86a4-2c953cb4be04",
+      currentPinIndex: 1,
+      totalPinIndex: 5,
+    },
+    {
+      width: 390,
+      height: 200,
+      borderRadius: 8,
+      title: "산책로 매력 포인트 1",
+      content: "이러쿵 저러쿵 뭔가 이쁜 부분이 있어서 사진이랑 남겨봅니다. 으하하하",
+      thumbnailURL:
+        "https://github.com/SeungHyune/first-script/assets/114329713/7842e910-8956-43f2-86a4-2c953cb4be04",
+      currentPinIndex: 1,
+      totalPinIndex: 5,
+    },
+    {
+      width: 390,
+      height: 200,
+      borderRadius: 8,
+      title: "산책로 매력 포인트 1",
+      content: "이러쿵 저러쿵 뭔가 이쁜 부분이 있어서 사진이랑 남겨봅니다. 으하하하",
+      thumbnailURL:
+        "https://github.com/SeungHyune/first-script/assets/114329713/7842e910-8956-43f2-86a4-2c953cb4be04",
+      currentPinIndex: 1,
+      totalPinIndex: 5,
+    },
+  ];
+
+  return (
+    <div style={{ width: 390 }}>
+      <Carousel
+        withoutControls={true}
+        slideIndex={args.slideIdx}
+        // beforeSlide={(_, v) => setSlideIdx(v)}
+      >
+        {data.map((d, i) => (
+          <PinDetailCard
+            content={d.content}
+            currentPinindex={i + 1}
+            thumbnailURL={d.thumbnailURL}
+            title={d.title}
+            totalPinIndex={data.length}
+            borderRadius={8}
+            height={200}
+            width={390}
+          />
+        ))}
+      </Carousel>
+      {/* CAROUSEL CONTROLLER */}
+      <div style={{ marginTop: 8 }}>
+        <DotIndicator
+          current={args.slideIdx}
+          length={data.length}
+        />
+      </div>
+    </div>
+  );
+};
