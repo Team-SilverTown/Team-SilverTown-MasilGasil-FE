1fcb7df3916d37a361553fe59b411c3400578b3e
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-19 19:10:09 +0900

    ✨feat: drag 상태에 대한 Store 생성

---
+++ src/components/MasilMap/store/useDraggingStore.ts
@@ -0,0 +1,20 @@
+import { GeoPosition } from "@/types/OriginDataType";
+import { create } from "zustand";
+
+interface UseDraggingStore {
+  isDragging: boolean;
+  draggingPosition: GeoPosition;
+
+  setIsDragging: (newState: boolean) => void;
+  setDraggingPosition: (newPosition: GeoPosition) => void;
+}
+
+const useUserLocationStore = create<UseDraggingStore>((set) => ({
+  isDragging: false,
+  draggingPosition: { lat: 0, lng: 0 },
+
+  setIsDragging: (newState) => set(() => ({ isDragging: newState })),
+  setDraggingPosition: (newPosition) => set(() => ({ draggingPosition: newPosition })),
+}));
+
+export default useUserLocationStore;
