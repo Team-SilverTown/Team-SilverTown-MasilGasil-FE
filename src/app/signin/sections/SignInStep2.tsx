8e63d05f6900d0ac767d1c9a4b457bfe0a9a9017
하송희 <99heesong@naver.com>
2024-02-20 17:53:13 +0900

    ✨feat: SignInStep2 나이 입력 폼 구현

--- src/app/signin/sections/SignInStep2.tsx
+++ src/app/signin/sections/SignInStep2.tsx
@@ -1,21 +1,23 @@
 "use client";
 
 import React, { useState } from "react";
-import { UseFormSetValue } from "react-hook-form";
+import { FieldErrors, UseFormSetValue, useForm, UseFormRegister } from "react-hook-form";
 
-import { Button } from "@/components";
 import useTheme from "@/lib/hooks/useTheme";
 
+import { Button, Input, InputLabel } from "@/components";
 import { SignInFormProps } from "../SignIn.controller";
 import * as S from "../SignIn.styles";
 
 interface SignInStep2Props {
   setValue: UseFormSetValue<SignInFormProps>;
+  register: UseFormRegister<SignInFormProps>;
+  errors: FieldErrors<SignInFormProps>;
 }
 
-const SignInStep2 = ({ setValue }: SignInStep2Props) => {
-  const [selectedSex, setSelectedSex] = useState<"male" | "female" | null>(null);
+const SignInStep2 = ({ setValue, register, errors }: SignInStep2Props) => {
   const theme = useTheme();
+  const [selectedSex, setSelectedSex] = useState<"male" | "female" | null>(null);
 
   const handleSexSelect = (sex: "male" | "female") => {
     setSelectedSex(sex);
@@ -23,7 +25,7 @@ const SignInStep2 = ({ setValue }: SignInStep2Props) => {
   };
 
   return (
-    <div className="h-full flex flex-col">
+    <div className="h-full">
       <S.GenderSection>
         <S.GenderTitle>성별</S.GenderTitle>
         <S.GenderButtonGroup>
@@ -47,6 +49,49 @@ const SignInStep2 = ({ setValue }: SignInStep2Props) => {
           </Button>
         </S.GenderButtonGroup>
       </S.GenderSection>
+
+      <S.BirthDateSection>
+        <S.BirthDateTitleWrapper>
+          <S.BirthDateTitle>나이</S.BirthDateTitle>
+          <InputLabel
+            text="14 ~ 100 사이의 값만 입력할 수 있어요!"
+            style={{ color: theme?.gray_200 }}
+          />
+        </S.BirthDateTitleWrapper>
+        <Input
+          required
+          type="number"
+          register={register("birthDate", {
+            required: "나이는 필수 값입니다.",
+            min: {
+              message: "14세 이상이어야 합니다",
+              value: 14,
+            },
+            max: {
+              message: "100세 이하여야 합니다",
+              value: 100,
+            },
+            // TODO: 동작하게 만들어야함
+            // pattern: {
+            //   message: "숫자만 입력 가능합니다",
+            //   value: /^[0-9]+$/,
+            // },
+          })}
+          placeholder="만 나이를 입력해주세요."
+          style={{
+            fontSize: "1.5rem",
+            lineHeight: "2rem",
+            color: theme?.gray_500,
+            margin: "1.4rem 0",
+          }}
+        />
+        <InputLabel
+          type="danger"
+          text={errors?.birthDate?.message}
+          fontSize={"1.5rem"}
+          style={{ position: "absolute" }}
+        />
+      </S.BirthDateSection>
     </div>
   );
 };
