b2f4e1e9ee818b30710b06304b47ef536cb00453
seunghyun <tmdgus7820@naver.com>
2024-02-20 23:45:38 +0900

    ✨feat: 쿼리스트링(keyword)에 따라서 타이틀 다르게 보이는 부분 구현

--- src/app/more/page.tsx
+++ src/app/more/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
+import { useSearchParams } from "next/navigation";
+import { TopNavigator } from "@/components/navigators/TopNavagtor";
+import { GoBackButton } from "@/components/navigators/TopNavagtor/components";
+import MoreListView from "./MoreList.view";
+
+const HEADER_TITLE: Record<string, string> = {
+  my_log: "내 최근 산책 기록",
+  my_post: "내가 작성한 산책로",
+  my_like: "내가 좋아하는 산책로",
+};
 
 const More = () => {
-  return <div>MorePage</div>;
+  const params = useSearchParams();
+  const queryString = params.get("keyword") || "더 보기 페이지";
+  return (
+    <>
+      <TopNavigator
+        leftChildren={<GoBackButton />}
+        title={HEADER_TITLE[queryString]}
+      />
+      <MoreListView />
+    </>
+  );
 };
 
 export default More;
