9f8e36d28dc960acc375706fb5392c16bb397520
하송희 <99heesong@naver.com>
2024-02-20 11:07:56 +0900

    ✨feat: SignInStep2 - 성별 버튼 선택 구현

--- src/app/signin/sections/SignInStep2.tsx
+++ src/app/signin/sections/SignInStep2.tsx
@@ -1,38 +1,52 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { UseFormSetValue } from "react-hook-form";
 
 import { Button } from "@/components";
 import useTheme from "@/lib/hooks/useTheme";
 
 import { SignInFormProps } from "../SignIn.controller";
+import * as S from "../SignIn.styles";
 
 interface SignInStep2Props {
   setValue: UseFormSetValue<SignInFormProps>;
 }
 
 const SignInStep2 = ({ setValue }: SignInStep2Props) => {
+  const [selectedSex, setSelectedSex] = useState<"male" | "female" | null>(null);
   const theme = useTheme();
 
-  const maleButtonClick = () => {
-    setValue("sex", "male");
-  };
-
-  const femaleButtonClick = () => {
-    setValue("sex", "female");
+  const handleSexSelect = (sex: "male" | "female") => {
+    setSelectedSex(sex);
+    setValue("sex", sex);
   };
 
   return (
-    <div className="bg-yellow-300 h-full">
-      SignInStep2
-      <Button
-        onClickHandler={maleButtonClick}
-        buttonColor={theme?.green_500}
-      >
-        남성
-      </Button>
-      <Button onClickHandler={femaleButtonClick}>여성</Button>
+    <div className="h-full flex flex-col">
+      <S.GenderSection>
+        <S.GenderTitle>성별</S.GenderTitle>
+        <S.GenderButtonGroup>
+          <Button
+            onClick={() => handleSexSelect("male")}
+            style={{
+              width: "50%",
+              backgroundColor: selectedSex === "male" ? theme?.green_500 : theme?.gray_200,
+            }}
+          >
+            남성
+          </Button>
+          <Button
+            onClick={() => handleSexSelect("female")}
+            style={{
+              width: "50%",
+              backgroundColor: selectedSex === "female" ? theme?.green_500 : theme?.gray_200,
+            }}
+          >
+            여성
+          </Button>
+        </S.GenderButtonGroup>
+      </S.GenderSection>
     </div>
   );
 };
