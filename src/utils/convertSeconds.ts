1df50dad820b746d0860cb3784a900fd6538f4df
seunghyune <114329713+SeungHyune@users.noreply.github.com>
2024-02-20 10:20:43 +0900

    [Feat] More List Card 컴포넌트 구현 (#66)
    * 🎨style: ListCard 컴포넌트 styled-components 구현 (layout 구현)
    
    * ✨feat: ListCard 컴포넌트 구현(만들어 놓은 styled-components 연동)
    
    * 🎨style: 레이아웃 간격 조정 및 제목, 내용 max-width 설정
    
    * 🚜change: ListCard 컴포넌트 공용 컴포넌트에 추가
    
    * ✨feat: ListCard 컴포넌트 Storybook 추가
    
    * ♻️refactor: convertSeconds(초를 시간으로 변환하는 함수), convertMeter(m를 km로 변환하는 함수) 수정
    
    * ♻️refactor: 좋아요에 대한 style 수정, isRecruitLayout 타입 추가, 함수 표현식으로 변경
    
    * 🎨style: ListCard Component에 cursor 속성 추가 (시간, 거리 간격 수정)
    
    * ✨feat: ListCard 컴포넌트 inline-style 사용 가능하도록 prop(style type) 추가
    
    * ♻️refactor: Utils 폴더로 분리
    
    * ♻️refactor: 전체적인 레이아웃 및 스타일 수정

---
+++ src/utils/convertSeconds.ts
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param sec 1초 단위로 전달되는 매개변수 입니다.
+ * @returns 산책 시간이 1분 미만이라면 1분을 보여줍니다.
+ *          산책 시간이 1시간 미만이라면 분만 보여줍니다.
+ *          산책 시간이 1시간 이상이라면 시간, 분을 같이 보여줍니다.
+ */
+
+export const convertSeconds = (sec: number): string => {
+  let minutes = Math.round(sec / 60);
+  let hours = Math.floor(minutes / 60);
+  minutes = minutes % 60;
+  if (sec < 60) return `1분`;
+  if (hours === 0) return `${minutes}분`;
+  return `${hours}시간 ${minutes}분`;
+};
