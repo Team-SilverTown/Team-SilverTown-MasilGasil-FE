e051bf6c9d8017cf2c6b685552d1aa6ae103213f
seunghyun <tmdgus7820@naver.com>
2024-02-22 01:33:26 +0900

    ♻️refactor: fetch 함수 분리 및 keyword 쿼리 스트링에 맞는 queryKey 동적 부여

--- src/app/more/MoreList.controller.tsx
+++ src/app/more/MoreList.controller.tsx
@@ -2,34 +2,16 @@
 
 import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import MoreListView from "./MoreList.view";
-
-interface getListRecommendsProps {
-  pageParam?: number;
-}
-
-/**
- *
- * @param pageParam 전달 받은 initialPageParam 데이터
- * @returns fetch를 통해 호출한 데이터의 json()을 리턴합니다.
- */
-export const getListRecommends = async ({ pageParam }: getListRecommendsProps) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${pageParam}`, {
-    next: {
-      tags: ["moreList", "recommends"],
-    },
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
-};
+import fetchMyLogList from "./lib/fetchMoreList";
+import useMoreListModel from "./MoreList.model";
 
 const MoreListController = async () => {
+  const { setSortTab, listInfo } = useMoreListModel();
+
   const queryClient = new QueryClient();
   await queryClient.prefetchInfiniteQuery({
-    queryKey: ["moreList", "recommends"],
-    queryFn: getListRecommends,
+    queryKey: ["moreList", listInfo.keyword],
+    queryFn: ({ pageParam, queryKey }) => fetchMyLogList({ pageParam, queryKey }),
     initialPageParam: 1,
   });
 
