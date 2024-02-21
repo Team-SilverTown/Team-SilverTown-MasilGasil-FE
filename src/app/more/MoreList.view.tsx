29eb0807a505a123bcca28dace59cfe25b5161bc
seunghyun <tmdgus7820@naver.com>
2024-02-22 02:03:49 +0900

    ♻️refactor: Page 페이지 서버 컴포넌트로 변경 및 TopNav 컴포넌트 MoreListView 내부로 수정

--- src/app/more/MoreList.view.tsx
+++ src/app/more/MoreList.view.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { Fragment, useEffect, useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { throttle } from "lodash";
 import * as S from "./MoreList.styles";
 import Skeleton from "./Skeleton/Skeleton";
 import { ListCard } from "@/components";
 import useMoreListModel from "./MoreList.model";
 import { SORT_DATA, SORT_TAB } from "./MoreList.constants";
+import { TopNavigator } from "@/components/navigators/TopNavagtor";
+import { GoBackButton } from "@/components/navigators/TopNavagtor/components";
 
 const MoreListView = () => {
   const { sortTab, setSortTab, infinityQuery, inViewResponse, listInfo } = useMoreListModel();
@@ -61,53 +63,59 @@ const MoreListView = () => {
   };
 
   return (
-    <S.MoreListLayout>
-      {isFilter && (
-        <S.MoreListFilter>
-          {SORT_DATA.map(({ title, keyword }, index) => (
-            <button
-              key={index}
-              type="button"
-              className={sortTab === keyword ? "selected" : ""}
-              title={keyword}
-              onClick={handleFilterToggle}
-            >
-              {title}
-            </button>
-          ))}
-        </S.MoreListFilter>
-      )}
-      <S.MoreListContainer>
-        {data?.pages.map((page, index) => (
-          <div key={index}>
-            {page.map((list) => (
-              <div key={list.id}>
-                <ListCard
-                  isRecruit={true}
-                  isLiked={true}
-                  likeCount={Math.round(1200 * Math.random())}
-                  title={`타이틀${list.id}`}
-                  content={`내용${list.id}`}
-                  totalTime={10000 * Math.random()}
-                  distance={100 * Math.random()}
-                  thumbnailURL=""
-                  address="테스트"
-                  style={{ marginBottom: "2rem" }}
-                />
-              </div>
+    <>
+      <TopNavigator
+        leftChildren={<GoBackButton />}
+        title={listInfo.name}
+      />
+      <S.MoreListLayout>
+        {isFilter && (
+          <S.MoreListFilter>
+            {SORT_DATA.map(({ title, keyword }, index) => (
+              <button
+                key={index}
+                type="button"
+                className={sortTab === keyword ? "selected" : ""}
+                title={keyword}
+                onClick={handleFilterToggle}
+              >
+                {title}
+              </button>
             ))}
-          </div>
-        ))}
-        {isFetchingNextPage ? (
-          <Skeleton repeat={5} />
-        ) : (
-          <div
-            ref={ref}
-            style={{ height: 50 }}
-          />
+          </S.MoreListFilter>
         )}
-      </S.MoreListContainer>
-    </S.MoreListLayout>
+        <S.MoreListContainer>
+          {data?.pages.map((page, index) => (
+            <div key={index}>
+              {page.map((list) => (
+                <div key={list.id}>
+                  <ListCard
+                    isRecruit={true}
+                    isLiked={true}
+                    likeCount={Math.round(1200 * Math.random())}
+                    title={`타이틀${list.id}`}
+                    content={`내용${list.id}`}
+                    totalTime={10000 * Math.random()}
+                    distance={100 * Math.random()}
+                    thumbnailURL=""
+                    address="테스트"
+                    style={{ marginBottom: "2rem" }}
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+          {isFetchingNextPage ? (
+            <Skeleton repeat={5} />
+          ) : (
+            <div
+              ref={ref}
+              style={{ height: 50 }}
+            />
+          )}
+        </S.MoreListContainer>
+      </S.MoreListLayout>
+    </>
   );
 };
 
--- src/app/more/page.tsx
+++ src/app/more/page.tsx
@@ -1,23 +1,7 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
-import { HEADER_TITLE } from "./MoreList.constants";
-import { TopNavigator } from "@/components/navigators/TopNavagtor";
-import { GoBackButton } from "@/components/navigators/TopNavagtor/components";
 import MoreListView from "./MoreList.view";
 
 const More = () => {
-  const params = useSearchParams();
-  const queryString = params.get("keyword") || "더 보기 페이지";
-  return (
-    <>
-      <TopNavigator
-        leftChildren={<GoBackButton />}
-        title={HEADER_TITLE[queryString]}
-      />
-      <MoreListView />
-    </>
-  );
+  return <MoreListView />;
 };
 
 export default More;
