375b3b24de4be4bf88460bc3c63d6b8ded586e06
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-21 12:35:48 +0900

    ✨feat: LoadingSpinner 구현

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -2,6 +2,17 @@
 
 import useLoadingSpinnerStore from "@/stores/ui/useLoadingSpinnerStore";
 import * as S from "./LoadingSpinner.styles";
+import Lottie from "react-lottie";
+import animationData from "./animationData.json";
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
 
 const LoadingSpinner = () => {
   const { isShow } = useLoadingSpinnerStore();
@@ -9,7 +20,16 @@ const LoadingSpinner = () => {
   if (!isShow) {
     return;
   }
-  return <S.LoadingSpinnerBackground>LoadingSpinner</S.LoadingSpinnerBackground>;
+
+  return (
+    <S.LoadingSpinnerBackground>
+      <Lottie
+        options={defaultOptions}
+        height={200}
+        width={240}
+      />
+    </S.LoadingSpinnerBackground>
+  );
 };
 
 export default LoadingSpinner;
