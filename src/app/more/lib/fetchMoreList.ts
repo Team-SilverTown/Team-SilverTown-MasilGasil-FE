817a4d81b035adc49e570bc068320b18c65b9532
seunghyun <tmdgus7820@naver.com>
2024-02-22 01:32:28 +0900

    ✨feat: fetchMoreList 함수로 분리 (공용 useInfiniteQuery fetch 함수)

---
+++ src/app/more/lib/fetchMoreList.ts
@@ -0,0 +1,27 @@
+interface fetchMoreListProps {
+  pageParam?: number;
+  queryKey: string[];
+}
+
+/**
+ *
+ * @param pageParam initialPageParam 데이터를 전달 받습니다.
+ * @param queryKey queryKey를 담은 배열을 전달 받습니다.
+ * @returns fetch를 통해 호출한 데이터의 json()을 리턴합니다.
+ */
+
+const fetchMoreList = async ({ pageParam, queryKey }: fetchMoreListProps) => {
+  const [_1, keyword] = queryKey;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${pageParam}`, {
+    next: {
+      tags: ["moreList", keyword],
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("MoreList 호출 에러!!");
+  }
+  return res.json();
+};
+
+export default fetchMoreList;
