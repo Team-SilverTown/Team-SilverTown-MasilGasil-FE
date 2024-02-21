8298a33a3ca760e96c184330281023179821d747
seunghyun <tmdgus7820@naver.com>
2024-02-22 01:30:15 +0900

    📝docs: 임시 테스트 데이터 추가

--- src/app/more/MoreList.constants.ts
+++ src/app/more/MoreList.constants.ts
@@ -1,5 +1,59 @@
+import { SearchKeywordType, SortDataType, SortTabType } from "./MoreList.types";
+
 export const HEADER_TITLE: Record<string, string> = {
   my_log: "내 최근 산책 기록",
   my_post: "내가 작성한 산책로",
   my_like: "내가 좋아하는 산책로",
+  area_popular: "우리 동네 인기 산책로",
+  total_popular: "인기 있는 산책로",
+  recent_post: "최근에 작성된 산책로",
+};
+
+export const SEARCH_KEYWORD: SearchKeywordType[] = [
+  {
+    name: "내 최근 산책 기록",
+    keyword: "my_log",
+    isFilter: true,
+    filter: "latest",
+  },
+  {
+    name: "내가 작성한 산책로",
+    keyword: "my_post",
+    isFilter: true,
+    filter: "latest",
+  },
+  {
+    name: "내가 좋아하는 산책로",
+    keyword: "my_like",
+    isFilter: true,
+    filter: "latest",
+  },
+  {
+    name: "인기 있는 산책로",
+    keyword: "posts",
+    isFilter: true,
+    filter: "popular",
+  },
+  {
+    name: "우리 동네 산책로",
+    keyword: "posts",
+    isFilter: true,
+    filter: "latest",
+  },
+];
+
+export const SORT_DATA: SortDataType[] = [
+  {
+    title: "최신순",
+    keyword: "latest",
+  },
+  {
+    title: "인기순",
+    keyword: "popular",
+  },
+];
+
+export const SORT_TAB: SortTabType = {
+  LATEST: "latest",
+  POPULAR: "popular",
 };
--- src/app/more/MoreList.types.ts
+++ src/app/more/MoreList.types.ts
@@ -6,3 +6,28 @@ export interface Dummy {
   email: string;
   body: string;
 }
+
+export type KeywordType =
+  | "my_log"
+  | "my_post"
+  | "my_like"
+  | "area_popular"
+  | "total_popular"
+  | "recent_post";
+
+export interface SearchKeywordType {
+  name: string;
+  keyword: string;
+  isFilter: boolean;
+  filter?: "latest" | "popular";
+}
+
+export interface SortDataType {
+  title: string;
+  keyword: string;
+}
+
+export interface SortTabType {
+  LATEST: "latest";
+  POPULAR: "popular";
+}
