1a45bb5c49019eda02bc46997aaf91da1f71380f
하송희 <99heesong@naver.com>
2024-02-21 11:36:44 +0900

    🎨style: SignInStep3 운동 강도 선택 스타일링

--- src/app/signin/SignIn.styles.ts
+++ src/app/signin/SignIn.styles.ts
@@ -1,6 +1,80 @@
 import { FONT_SIZE, FONT_WEIGHT } from "@/styles/theme";
 import styled from "styled-components";
 
+export const GenderSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  font-weight: ${FONT_WEIGHT.BOLD};
+  font-size: 2rem;
+  margin-top: 3rem;
+`;
+
+export const GenderTitle = styled.span``;
+
+export const GenderButtonGroup = styled.div`
+  display: flex;
+  justify-content: space-around;
+  gap: 1.2rem;
+  margin: 1.4rem 0;
+  color: ${({ theme }) => theme.white};
+  cursor: pointer;
+`;
+
+export const BirthDateSection = styled.section`
+  margin-top: 3rem;
+`;
+
+export const BirthDateTitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+`;
+
+export const BirthDateTitle = styled.span`
+  display: block;
+  font-size: 2rem;
+  font-weight: ${FONT_WEIGHT.BOLD};
+  width: 4.5rem;
+`;
+
+export const PhysicalSection = styled.section`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 3rem;
+  gap: 1.2rem;
+`;
+
+export const Group = styled.div`
+  width: 50%;
+`;
+
+export const GroupTitle = styled.span`
+  font-size: 2rem;
+  font-weight: ${FONT_WEIGHT.BOLD};
+`;
+
+export const ExerciseIntensityTitleSection = styled.section`
+  margin: 3rem 0;
+`;
+
+export const Title = styled.span`
+  display: block;
+  font-size: 2rem;
+  font-weight: ${FONT_WEIGHT.BOLD};
+`;
+
+export const ExerciseIntensityOption = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.6rem;
+  margin: 1.6rem;
+`;
+
+export const RadioCircle = styled.label`
+  cursor: "pointer";
+  margin-left: 0.6rem;
+`;
+
 export const PolicyLable = styled.label`
   margin-left: 1rem;
   cursor: pointer;
