670481c3f543a2004c41f09bbf048ecd0585ab45
seunghyun <tmdgus7820@naver.com>
2024-02-22 01:30:56 +0900

    ✨feat: 모든 더 보기 페이지 query에 맞게 데이터 가져오도록 작업(정렬 상태를 관리하는 상태 추가)

--- src/app/more/MoreList.model.ts
+++ src/app/more/MoreList.model.ts
@@ -1,9 +1,25 @@
+"use client";
+
 import { InfiniteData, useInfiniteQuery } from "@tanstack/react-query";
-import { Dummy } from "./MoreList.types";
-import { getListRecommends } from "./MoreList.controller";
+import { Dummy, KeywordType } from "./MoreList.types";
 import { useInView } from "react-intersection-observer";
+import fetchMyLogList from "./lib/fetchMoreList";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { SEARCH_KEYWORD } from "./MoreList.constants";
 
 const useMoreListModel = () => {
+  const searchParams = useSearchParams();
+  const searchKeyword = searchParams.get("keyword") as KeywordType;
+
+  const listInfo = SEARCH_KEYWORD.filter(({ keyword }) => keyword === searchKeyword)[0];
+
+  if (!listInfo) {
+    throw new Error("'keyword' 파라미터가 없습니다.");
+  }
+
+  const [sortTab, setSortTab] = useState(listInfo.filter);
+
   const infinityQuery = useInfiniteQuery<
     Dummy[],
     Object,
@@ -11,23 +27,26 @@ const useMoreListModel = () => {
     [_1: string, _2: string],
     number
   >({
-    queryKey: ["moreList", "recommends"],
-    queryFn: getListRecommends,
+    queryKey: ["moreList", listInfo.keyword],
+    queryFn: ({ pageParam, queryKey }) => fetchMyLogList({ pageParam, queryKey }),
     initialPageParam: 1,
-    getNextPageParam: (lastPage, allPage) => (allPage.length * 5) / 5 + 1,
+    getNextPageParam: (_, allPage) => (allPage.length * 5) / 5 + 1,
     staleTime: 60 * 1000,
     gcTime: 300 * 1000,
   });
 
   // 반환 받은 ref를 통해 화면에 닿았는지 체크하는 hook
-  const infinityRef = useInView({
+  const inViewResponse = useInView({
     threshold: 0,
     delay: 0,
   });
 
   return {
+    sortTab,
+    setSortTab,
     infinityQuery,
-    infinityRef,
+    inViewResponse,
+    listInfo,
   };
 };
 
