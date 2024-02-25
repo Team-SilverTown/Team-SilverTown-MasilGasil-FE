accf97ae2fcfd37b34cb8490b80a082d044c2e7d
joseunghyeon <tmdgus7820@naver.com>
2024-02-26 01:29:06 +0900

    🎨style: InputUpload 컴포넌트 기본 Style 정의

--- src/components/InputUpload/InputUpload.styles.ts
+++ src/components/InputUpload/InputUpload.styles.ts
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
-export const InputUploadLayout = styled.div``;
+interface InputUploadPreviewProps {
+  $position: "relative" | "absolute";
+}
+
+export const InputUploadLayout = styled.div`
+  position: relative;
+`;
 
 export const InputUpload = styled.input``;
 
@@ -8,6 +14,19 @@ export const InputUploadChildren = styled.div`
   cursor: pointer;
 `;
 
-export const InputUploadPreview = styled.div`
+export const InputUploadPreview = styled.div<InputUploadPreviewProps>`
+  position: ${(props) => props.$position};
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  max-width: 100%;
+  max-height: 100%;
   cursor: pointer;
+
+  img {
+    object-fit: cover;
+    object-position: center;
+    height: 100%;
+  }
 `;
