2730083172c85dfc243c3fcf2b1effde86022c4d
Ha Songhee <74524184+Songhee99@users.noreply.github.com>
2024-02-27 00:42:22 +0900

    [Feat] SignIn Step2 구현 (#80)
    * 📝docs: PR 및 Issue 양식 추가
    
    * 📝docs: Issue 양식 추가
    
    * ✨feat: SignInStep2 - 성별 버튼 선택 구현
    
    * 🎨style: SignInStep2 - 성별 버튼 스타일링
    
    * ✨feat: SignInStep2 나이 입력 폼 구현
    
    * 🎨style: SignInStep2 나이 입력 폼 스타일링
    
    * ✨feat: SignInStep2 - 키, 체중 입력 폼 구현
    
    * 🎨style: SignInStep2 - 키, 체중 입력 폼 스타일링
    
    * ✨feat: SignInStep2 validation 구현
    
    * ♻️refactor: SignInStep2 상태관리 SignIn.model로 이동
    
    * ♻️refactor: 컴포넌트명 수정, map 이용해 성별 버튼 구현
    
    * ♻️refactor: Input, InputLabel 상단에 별도 분리
    
    * ✨feat: pr 리뷰 반영 (constants 분리, unitLabel 추가)
    
    * ✨feat: pr 리뷰 반영 (태그 변경, interface로 타입 명시)
    
    * 🗑️remove: type=number 이기 때문에 필요 없는 유효성 검사 구문 삭제
    
    * ✨feat: 상수 파일로 validation 분리 관리
    
    ---------
    
    Co-authored-by: Jaewoong Hwang <95916813+w00ngja@users.noreply.github.com>

--- src/app/signin/SignIn.styles.ts
+++ src/app/signin/SignIn.styles.ts
@@ -1,7 +1,76 @@
 import { FONT_SIZE, FONT_WEIGHT } from "@/styles/theme";
 import styled from "styled-components";
 
-export const PolicyLable = styled.label`
+export const Title = styled.strong`
+  display: block;
+  font-size: 2rem;
+`;
+
+export const GenderSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  font-size: 2rem;
+  margin-top: 3rem;
+`;
+
+export const GenderButtonGroup = styled.div`
+  display: flex;
+  justify-content: space-around;
+  gap: 1.2rem;
+  margin: 1.4rem 0;
+  color: ${({ theme }) => theme.white};
+  font-weight: ${FONT_WEIGHT.BOLD};
+`;
+
+export const BirthDateSection = styled.section`
+  margin-top: 3rem;
+`;
+
+export const BirthDateTitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+`;
+
+export const PhysicalSection = styled.section`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 3rem;
+  gap: 1.2rem;
+`;
+
+export const PhysicalGroup = styled.div`
+  width: 50%;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+  width: 100%;
+
+  input[type="number"] {
+    -webkit-appearance: none;
+    margin: 0;
+
+    &::-webkit-inner-spin-button,
+    &::-webkit-outer-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+  } //Input type=number의 기본 우측 화살표 제거
+`;
+
+export const UnitLabel = styled.span`
+  position: absolute;
+  right: 1.2rem;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 1.8rem;
+  font-weight: ${FONT_WEIGHT.REGULAR};
+`;
+
+export const PolicyLabel = styled.label`
   margin-left: 1rem;
   cursor: pointer;
 `;
