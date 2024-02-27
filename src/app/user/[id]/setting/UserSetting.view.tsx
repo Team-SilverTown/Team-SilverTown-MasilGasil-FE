06230e0a746eb289d66d7eb1f1c310066798e789
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-27 16:43:23 +0900

    ✨feat: layout 테스트

--- src/app/user/[id]/setting/UserSetting.view.tsx
+++ src/app/user/[id]/setting/UserSetting.view.tsx
@@ -1,15 +1,27 @@
+"use client";
+
 import * as GS from "@/styles/GlobalStyle";
+import * as S from "./UserSetting.styles";
 
 import { TopNavigator } from "@/components/navigators/TopNavagtor";
 import { GoBackButton } from "@/components/navigators/TopNavagtor/components";
 
 const UserSettingView = () => {
   return (
-    <GS.CommonContainer>
+    <GS.CommonContainer style={{ height: "100%" }}>
       <TopNavigator
         leftChildren={<GoBackButton />}
         title={"설정"}
       />
+
+      <S.UserSettingLayout>
+        <div>설정</div>
+
+        <div>토글 1</div>
+        <div>토글 2</div>
+
+        <div></div>
+      </S.UserSettingLayout>
     </GS.CommonContainer>
   );
 };
