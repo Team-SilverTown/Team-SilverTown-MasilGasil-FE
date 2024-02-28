4843e8dfab9160c30502ac8534ce15464fbc09d9
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-28 14:18:27 +0900

    🗑️remove: 미사용 코드 제거

--- src/app/log/record/LogRecord.types.ts
+++ src/app/log/record/LogRecord.types.ts
@@ -1,4 +1,3 @@
-import { MasilRecordRequest } from "@/types/Request";
 import { Dispatch, SetStateAction } from "react";
 
 export type LogPageStep = "LOG_RECORD_STANDBY" | "LOG_RECORD_RECORDING" | "LOG_RECORD_EDITING";
@@ -6,11 +5,5 @@ export type LogPageStep = "LOG_RECORD_STANDBY" | "LOG_RECORD_RECORDING" | "LOG_R
 export type OnErrorWatcher = (error: GeolocationPositionError) => void;
 
 export type UpdateUserLocation = (geo: GeolocationPosition) => void;
-
-export type SetWatchCode = Dispatch<SetStateAction<number>>;
-
-export type SetPageStep = Dispatch<SetStateAction<LogPageStep>>;
-export type SetIsActiveExitAni = Dispatch<SetStateAction<boolean>>;
-export type SetLogData = Dispatch<SetStateAction<MasilRecordRequest>>;
 
 export type SetIsMapResizing = Dispatch<SetStateAction<boolean>>;
