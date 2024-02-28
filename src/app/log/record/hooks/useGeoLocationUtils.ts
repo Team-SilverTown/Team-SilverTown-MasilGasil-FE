03b98648805b317d516e5d28e3d87f5f0ff993b4
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-28 15:08:51 +0900

    ♻️refactor: useGeoLocationUtils 개선

--- src/app/log/record/hooks/useGeoLocationUtils.ts
+++ src/app/log/record/hooks/useGeoLocationUtils.ts
@@ -3,33 +3,35 @@ import useUserLocationStore from "@/stores/useUserLocationStore";
 import { throttle } from "lodash";
 import { useRef } from "react";
 import { LOG_RECORD_MESSAGE } from "../LogRecord.constants";
+import { useRouter } from "next/navigation";
 
 const useGeoLocationUtils = () => {
   const { setUserLocation } = useUserLocationStore();
   const { setModalView, openModal } = useUI();
+  const router = useRouter();
+
   /**
    * @summary watcher가 오류가 발생했을때 수행할 동작을 위한 함수입니다.
+   *
+   * @param code 에러가 발생한 코드를 확인할 수 있습니다.
    * @param PERMISSION_DENIED 사용자가 위치 서비스를 동의 하지 않았는지를 파악할때 code와 비교하는 용도로 사용되어집니다.
    */
   const onErrorWatch = ({ code, PERMISSION_DENIED }: GeolocationPositionError) => {
     setModalView("LOG_RECORD_ALERT_VIEW");
 
     if (code === PERMISSION_DENIED) {
-      /*
-        TODO
-        추후 멘트 수정 
-        */
       openModal({
         message: LOG_RECORD_MESSAGE.ERROR.WATCH_PERMISSION_DENIED,
       });
+      router.back();
+      return;
     }
-    /*
-      TODO
-      추후 멘트 수정 
-      */
+
     openModal({
       message: LOG_RECORD_MESSAGE.ERROR.WATCH_ERROR,
     });
+    router.back();
+    return;
   };
 
   /**
