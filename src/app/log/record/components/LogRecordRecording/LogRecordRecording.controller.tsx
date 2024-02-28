52e79d942f5c2ee0bc237373a0e01d3ee104d395
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-28 14:17:48 +0900

    ♻️refactor: Step2 Context 적용 및 정리

--- src/app/log/record/components/LogRecordRecording/LogRecordRecording.controller.tsx
+++ src/app/log/record/components/LogRecordRecording/LogRecordRecording.controller.tsx
@@ -1,44 +1,24 @@
-import { Dispatch, SetStateAction, useEffect, useRef } from "react";
+import { useEffect } from "react";
 import LogRecordRecordingView from "./LogRecordRecording.view";
-import { OnErrorWatcher, SetLogData, SetPageStep, UpdateUserLocation } from "../../LogRecord.types";
-import { throttle } from "lodash";
-import { MasilRecordRequest } from "@/types/Request";
+import { OnErrorWatcher, UpdateUserLocation } from "../../LogRecord.types";
+
 import { useUI } from "@/components/uiContext/UiContext";
-import getTwoPointDistance from "../../utils/getTwoPointDistance";
+
 import { LOG_RECORD_MESSAGE } from "../../LogRecord.constants";
 import useLogRecordContext from "../../context/LogRecordContext";
 
 interface LogRecordRecordingControllerProps {
-  logData: MasilRecordRequest;
-
-  setLogData: SetLogData;
-  setPageStep: SetPageStep;
   onErrorWatcher: OnErrorWatcher;
   updateUserLocation: UpdateUserLocation;
-
-  setIsActiveExitAni: Dispatch<SetStateAction<boolean>>;
 }
 
 const LogRecordRecordingController = ({
-  logData,
-  setLogData,
-  setPageStep,
   onErrorWatcher,
   updateUserLocation,
-
-  setIsActiveExitAni,
 }: LogRecordRecordingControllerProps) => {
   const { openModal, setModalView, closeModal } = useUI();
-  const { increaseTimer, updatePath } = useLogRecordContext();
-
-  // Context Reducer
-  /**
-   * @summary watch의 success 콜백에 들어가는 함수로 경로 값을 추가시켜줍니다.
-   *
-   * 단, throttle을 통해 5초에 한번 실행.
-   *
-   * 실행된 후 이전 경로보다 일정거리 이하 혹은 이상인 경우 종료.
-   */
+  const { increaseTimer, updatePath, logData, setPageStep, setIsActiveExitAnimation } =
+    useLogRecordContext();
 
   useEffect(() => {
     const watchCode = navigator.geolocation.watchPosition(
@@ -58,11 +38,14 @@ const LogRecordRecordingController = ({
     };
   }, []);
 
+  /**
+   * @summary 기록을 완료할때 Modal을 제공합니다.
+   */
   const handleClickCompleteRecord = () => {
     setModalView("LOG_RECORD_CONFIRM_VIEW");
     openModal({
       onClickAccept: () => {
-        setIsActiveExitAni(true);
+        setIsActiveExitAnimation(true);
         setPageStep("LOG_RECORD_EDITING");
         closeModal();
       },
