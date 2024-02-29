735d715e690a3c61c47825efa6cade73f984d51c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-29 22:36:01 +0900

    🐛fix: return 누락 추가

--- src/app/user/[id]/setting/edit/components/UserEditInput/UserEditInput.tsx
+++ src/app/user/[id]/setting/edit/components/UserEditInput/UserEditInput.tsx
@@ -24,32 +24,34 @@ const UserEditInput = ({
   register,
   errorsMessage,
 }: UserEditInputProps) => {
-  <>
-    <GS.UserEditTitle>{title}</GS.UserEditTitle>
+  return (
+    <>
+      <GS.UserEditTitle>{title}</GS.UserEditTitle>
 
-    <Input
-      type="number"
-      register={register(type, {
-        required: requiredMessage,
-      })}
-      placeholder={placeholder}
-      style={{
-        lineHeight: "2rem",
-        width: "100%",
-        fontSize: "1.5rem",
-        fontWeight: FONT_WEIGHT.SEMIBOLD,
-      }}
-    />
+      <Input
+        type="number"
+        register={register(type, {
+          required: requiredMessage,
+        })}
+        placeholder={placeholder}
+        style={{
+          lineHeight: "2rem",
+          width: "100%",
+          fontSize: "1.5rem",
+          fontWeight: FONT_WEIGHT.SEMIBOLD,
+        }}
+      />
 
-    <GS.UserEditWarning>
-      {errorsMessage && (
-        <InputLabel
-          text={errorsMessage}
-          type="danger"
-        />
-      )}
-    </GS.UserEditWarning>
-  </>;
+      <GS.UserEditWarning>
+        {errorsMessage && (
+          <InputLabel
+            text={errorsMessage}
+            type="danger"
+          />
+        )}
+      </GS.UserEditWarning>
+    </>
+  );
 };
 
 export default UserEditInput;
