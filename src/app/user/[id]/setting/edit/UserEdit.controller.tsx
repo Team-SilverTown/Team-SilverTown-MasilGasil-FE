a9687c84a562f31d65b21104a212db6de9710c0b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-29 13:43:51 +0900

    ✨feat: 임시 데이터 모델 생성

--- src/app/user/[id]/setting/edit/UserEdit.controller.tsx
+++ src/app/user/[id]/setting/edit/UserEdit.controller.tsx
@@ -1,12 +1,34 @@
 "use client";
 
+import { useForm } from "react-hook-form";
 import UserEditView from "./UserEdit.view";
 
 interface UserEditControllerProps {
   userId: string;
 }
 
+interface UserData {
+  nickname: string;
+  sex: "male" | "female";
+  age: number;
+  height: number;
+  weight: number;
+}
+
+const FORM_DEFAULT_VALUE: UserData = {
+  nickname: "",
+  sex: "male",
+  age: 12,
+  height: 182,
+  weight: 150,
+};
+
 const UserEditController = ({ userId }: UserEditControllerProps) => {
+  const {} = useForm<UserData>({
+    mode: "onChange",
+    defaultValues: FORM_DEFAULT_VALUE,
+  });
+
   // 추후 userId를 이용해 사용자 정보를 호출할 예정
   console.log(userId);
   return <UserEditView />;
