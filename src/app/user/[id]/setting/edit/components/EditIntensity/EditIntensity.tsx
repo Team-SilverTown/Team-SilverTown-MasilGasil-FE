c0615e65dd31aff29a9ba085ba170e093cb1d09e
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-03-01 00:23:15 +0900

    ♻️refactor: 공용 변수 상수화

--- src/app/user/[id]/setting/edit/components/EditIntensity/EditIntensity.tsx
+++ src/app/user/[id]/setting/edit/components/EditIntensity/EditIntensity.tsx
@@ -4,6 +4,7 @@ import * as S from "./EditIntensity.styles";
 import useTheme from "@/lib/hooks/useTheme";
 import { UseFormRegister, UseFormRegisterReturn } from "react-hook-form";
 import { IntensityType, UserEditData } from "../../UserEdit.types";
+import { INTENSITY_OPTIONS } from "@/constants/variable";
 
 interface EditIntensityProps {
   register: UseFormRegister<UserEditData>;
@@ -11,14 +12,6 @@ interface EditIntensityProps {
 }
 
 const EditIntensity = ({ register, selectedIntensity }: EditIntensityProps) => {
-  const exerciseIntensity = [
-    { label: "좌식", value: "SUPER_LOW" },
-    { label: "가벼운 활동(가벼운 운동/스포츠 3-5일/주)", value: "LOW" },
-    { label: "적당히 활동적(중간 정도의 운동/스포츠 3-5일/주)", value: "MIDDLE" },
-    { label: "활동적(일주일에 6-7일 격렬한 운동/스포츠)", value: "HIGH" },
-    { label: "매우 활독적인 경우 (매우 힘든 운동/스포츠 및 육체 노동)", value: "SUPER_HIGH" },
-  ];
-
   return (
     <GS.UserEditSectionContainer>
       <S.IntensityTitle>평소 운동 강도</S.IntensityTitle>
@@ -27,7 +20,7 @@ const EditIntensity = ({ register, selectedIntensity }: EditIntensityProps) => {
       </S.IntensityDescription>
 
       <S.IntensityOptionList>
-        {exerciseIntensity.map(({ label, value }) => (
+        {INTENSITY_OPTIONS.map(({ label, value }) => (
           <IntensityItem
             key={value}
             value={value}
