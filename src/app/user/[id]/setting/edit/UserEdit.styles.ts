54865b97d1cd1fc9eaa86e732d63ea698a295ca1
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-29 17:10:36 +0900

    🎨style: EditNickname 스타일 생성

--- src/app/user/[id]/setting/edit/UserEdit.styles.ts
+++ src/app/user/[id]/setting/edit/UserEdit.styles.ts
@@ -1,3 +1,4 @@
+import { FONT_SIZE, FONT_WEIGHT } from "@/styles/theme";
 import styled from "styled-components";
 
 export const UserEditLayout = styled.form`
@@ -10,4 +11,14 @@ export const UserEditLayout = styled.form`
   gap: 2rem;
 
   border: 1px solid red;
+`;
+
+export const UserEditTitle = styled.h6`
+  margin-bottom: 1.6rem;
+  padding-left: 0.6rem;
+
+  font-size: ${FONT_SIZE.H6};
+  font-weight: ${FONT_WEIGHT.BOLD};
+
+  user-select: none;
 `;
--- src/app/user/[id]/setting/edit/components/EditNickname/EditNickname.tsx
+++ src/app/user/[id]/setting/edit/components/EditNickname/EditNickname.tsx
@@ -1,6 +1,11 @@
+import * as S from "./EditNickname.styles";
+import * as GS from "../../UserEdit.styles";
+
 import { UseFormRegister } from "react-hook-form";
 import { UserEditData } from "../../UserEdit.types";
-import { Input } from "@/components";
+import { Button, Input, InputLabel } from "@/components";
+import useTheme from "@/lib/hooks/useTheme";
+import { FONT_WEIGHT } from "@/styles/theme";
 
 interface EditNicknameProps {
   register: UseFormRegister<UserEditData>;
@@ -9,17 +14,46 @@ interface EditNicknameProps {
 }
 
 const EditNickname = ({ register, onChangeNickname }: EditNicknameProps) => {
+  const theme = useTheme();
+
+  if (!theme) {
+    return;
+  }
   return (
-    <Input
-      register={register("nickname", {
-        required: "변경하실 닉네임을 입력해주세요.",
-        minLength: { message: "2글자 이상을 입력해 주세요.", value: 2 },
-        maxLength: { message: " 10글자 이하로 입력해 주세요.", value: 10 },
-      })}
-      type="text"
-      placeholder="수정하실 닉네임을 입력해주세요!"
-      onChange={onChangeNickname}
-    />
+    <S.EditNickNameContainer>
+      <GS.UserEditTitle>닉네임</GS.UserEditTitle>
+
+      <S.EditNicknameActions>
+        <Input
+          register={register("nickname", {
+            required: "변경하실 닉네임을 입력해주세요.",
+            minLength: { message: "2글자 이상을 입력해 주세요.", value: 2 },
+            maxLength: { message: " 10글자 이하로 입력해 주세요.", value: 10 },
+          })}
+          type="text"
+          placeholder="수정하실 닉네임을 입력해주세요!"
+          onChange={onChangeNickname}
+        />
+
+        <Button
+          width={"16rem"}
+          buttonColor={theme.green_500}
+          textColor={theme.text_secondary_color}
+          useRipple={true}
+          rippleColor={theme.text_secondary_color + 50}
+          style={{ whiteSpace: "nowrap", fontWeight: FONT_WEIGHT.SEMIBOLD, userSelect: "none" }}
+        >
+          중복 확인
+        </Button>
+      </S.EditNicknameActions>
+
+      <S.EditNicknameWarningWrapper>
+        <InputLabel
+          text="이것은 경고 문자"
+          type="danger"
+        />
+      </S.EditNicknameWarningWrapper>
+    </S.EditNickNameContainer>
   );
 };
 
