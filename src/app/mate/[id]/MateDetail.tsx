8ead9623d8f2202eb64c6ba5863095e3cb35d2a8
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-03-01 23:36:34 +0900

    ✨feat: TopNavigator GoBackButton 추가

--- src/app/mate/[id]/MateDetail.tsx
+++ src/app/mate/[id]/MateDetail.tsx
@@ -4,13 +4,17 @@ import { TopNavigator } from "@/components/navigators/TopNavigator";
 import { CommonContainerTailwind } from "@/styles/GlobalStyle";
 import Divider from "@/components/Divider/Divider";
 import { MateEvaluation, MateInfo, MateMembers } from "./components";
+import { GoBackButton } from "@/components/navigators/TopNavigator/components";
 
 interface MateDetailProps {}
 
 const MateDetail = ({}: MateDetailProps) => {
   return (
     <>
-      <TopNavigator title="메이트 모집" />
+      <TopNavigator
+        title="메이트 모집"
+        leftChildren={<GoBackButton />}
+      />
       <section className={CommonContainerTailwind}>
         <article className={S.MateDetailLayout}>
           <MateInfo />
