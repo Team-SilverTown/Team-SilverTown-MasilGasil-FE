7d48a1405af12a71e3630f19b6da7a12648c055e
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-03-02 00:58:30 +0900

    ♻️refactor: MateEvaluation 개선

--- src/app/mate/[id]/components/MateEvaluation/MateEvaluation.tsx
+++ src/app/mate/[id]/components/MateEvaluation/MateEvaluation.tsx
@@ -7,34 +7,33 @@ interface MateEvaluationProps {
   nickName: string;
 }
 
+const evaluationList: {
+  type: "kind" | "time" | "bad";
+  text: string;
+}[] = [
+  { type: "kind", text: "친절해요 😊" },
+  { type: "time", text: "시간약속을 잘지켜요! 👍" },
+  { type: "bad", text: "부담스러워요 .. 🥹" },
+];
+
 const MateEvaluation = ({ authorEvaluation, nickName }: MateEvaluationProps) => {
-  const { kind, time, bad } = authorEvaluation;
   return (
     <article className={GS.MateInformationContainer}>
       <h6 className={GS.MateDetailInformationTitle}>{`${nickName}님이 받은 매너 평가`}</h6>
 
       <ul className="w-full py-6 flex flex-col gap-6">
-        <li className={S.EvaluationItem}>
-          <p className={S.EvaluationTitle}>친절해요 😊</p>
-          <p className={S.EvaluationText}>
-            {kind}
-            <span className={S.EvaluationUnit}>명</span>
-          </p>
-        </li>
-        <li className={S.EvaluationItem}>
-          <p className={S.EvaluationTitle}>시간약속을 잘지켜요! 👍</p>
-          <p className={S.EvaluationText}>
-            {time}
-            <span className={S.EvaluationUnit}>명</span>
-          </p>
-        </li>
-        <li className={S.EvaluationItem}>
-          <p className={S.EvaluationTitle}>부담스러워요 .. 🥹</p>
-          <p className={S.EvaluationText}>
-            {bad}
-            <span className={S.EvaluationUnit}>명</span>
-          </p>
-        </li>
+        {evaluationList.map(({ text, type }) => (
+          <li
+            className={S.EvaluationItem}
+            key={type}
+          >
+            <p className={S.EvaluationTitle}>{text}</p>
+            <p className={S.EvaluationText}>
+              {authorEvaluation[type]}
+              <span className={S.EvaluationUnit}>명</span>
+            </p>
+          </li>
+        ))}
       </ul>
     </article>
   );
