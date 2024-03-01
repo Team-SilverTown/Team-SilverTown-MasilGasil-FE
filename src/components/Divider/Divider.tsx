9a6568afa809e10bd23beb9e131e14cde7567ec0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-03-01 23:34:11 +0900

    🐛fix: tailwind 색상 적용 오류 수정

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,13 +1,12 @@
 interface DividerProps {
   isColumn?: boolean;
-  color?: string;
 }
 
-const Divider = ({ isColumn = false, color = "#E3E3E3" }: DividerProps) => {
+const Divider = ({ isColumn = false }: DividerProps) => {
   const columnStyle = "w-1 h-full";
   const rowStyle = "w-full h-1";
 
-  return <div className={`${isColumn ? columnStyle : rowStyle} rounded bg-[${color}]`} />;
+  return <div className={`${isColumn ? columnStyle : rowStyle} rounded bg-[#E3E3E3]`} />;
 };
 
 export default Divider;
