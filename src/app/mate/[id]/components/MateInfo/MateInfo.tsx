f591d80aa8c8933f42913f45aa43e437fb0a388d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-03-02 14:24:38 +0900

    ✨feat: MateInfo의 UserInfo 구역 생성

--- src/app/mate/[id]/components/MateInfo/MateInfo.tsx
+++ src/app/mate/[id]/components/MateInfo/MateInfo.tsx
@@ -1,9 +1,16 @@
 import * as GS from "../../MateDetail.styles";
 
+import Avatar from "@/components/Avatar/Avatar";
+import { MateToggleMenu } from "./components";
+
 const MateInfo = () => {
   return (
     <article className={GS.MateInformationContainer}>
-      <div></div>
+      <div className="w-full flex items-center gap-6">
+        <Avatar size="sm" />
+        <p className="grow text-medium font-medium whitespace-nowrap">재영</p>
+        <MateToggleMenu />
+      </div>
     </article>
   );
 };
---
+++ src/app/mate/[id]/components/MateInfo/components/index.ts
@@ -0,0 +1,1 @@
+export { default as MateToggleMenu } from "./MateToggleMenu/MateToggleMenu";
