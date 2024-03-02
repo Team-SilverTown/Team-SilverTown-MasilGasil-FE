a75da116059514feb3208a7a9f9864fedeada6eb
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-03-02 15:41:16 +0900

    ✨feat: MateToggleMenu 동작 및 애니메이션 구현

--- src/app/mate/[id]/components/MateInfo/components/MateToggleMenu/MateToggleMenu.styles.ts
+++ src/app/mate/[id]/components/MateInfo/components/MateToggleMenu/MateToggleMenu.styles.ts
@@ -2,9 +2,20 @@ import { FONT_SIZE, FONT_WEIGHT, Z_INDEX } from "@/styles/theme";
 import { motion } from "framer-motion";
 import styled from "styled-components";
 
-export const MateToggleMenuLayout = styled(motion.ul)`
+export const MateToggleMenuLayout = styled(motion.div)`
   width: 9rem;
   height: 10.6rem;
+
+  position: absolute;
+  right: 1rem;
+  bottom: -10rem;
+
+  z-index: ${Z_INDEX.MATE_TOGGLE_MENU};
+`;
+
+export const MateToggleMenuContainer = styled(motion.ul)`
+  width: 100%;
+  height: 100%;
   padding: 1rem;
 
   display: flex;
@@ -13,13 +24,11 @@ export const MateToggleMenuLayout = styled(motion.ul)`
   align-items: center;
   gap: 1.6rem;
 
-  position: absolute;
-  right: 1rem;
-  bottom: -10rem;
-
   border-radius: 0.8rem;
   background-color: ${({ theme }) => theme.background_color};
   box-shadow: 0 0 1rem 0.2rem rgba(0, 0, 0, 0.1);
+
+  overflow: hidden;
 `;
 
 export const MateToggleMenuItem = styled.li`
@@ -31,5 +40,4 @@ export const MateToggleMenuItem = styled.li`
   font-weight: ${FONT_WEIGHT.SEMIBOLD};
 
   cursor: pointer;
-  z-index: ${Z_INDEX.MATE_TOGGLE_MENU};
 `;
--- src/app/mate/[id]/components/MateInfo/components/MateToggleMenu/MateToggleMenu.tsx
+++ src/app/mate/[id]/components/MateInfo/components/MateToggleMenu/MateToggleMenu.tsx
@@ -4,26 +4,43 @@ import Divider from "@/components/Divider/Divider";
 import * as S from "./MateToggleMenu.styles";
 import { DotMenu, Trash } from "@/components/icons";
 import EditPencil from "@/components/icons/EditPencil";
+import useToggle from "@/hooks/useToggle";
+import { AnimatePresence } from "framer-motion";
 
 interface MateToggleMenuProps {
   postId: string;
 }
 
 const MateToggleMenu = ({ postId }: MateToggleMenuProps) => {
+  const { isToggle, handleToggle, toggleRef } = useToggle();
   return (
     <>
-      <DotMenu className="w-8 h-8 mr-4" />
+      <DotMenu
+        className="w-8 h-8 mr-4"
+        onClick={handleToggle}
+        style={{ cursor: "pointer" }}
+      />
 
-      <S.MateToggleMenuLayout>
-        <S.MateToggleMenuItem>
-          <EditPencil className="w-6 h-6" />
-          수정
-        </S.MateToggleMenuItem>
-        <Divider />
-        <S.MateToggleMenuItem>
-          <Trash className="w-6 h-6" /> 삭제
-        </S.MateToggleMenuItem>
-      </S.MateToggleMenuLayout>
+      <AnimatePresence>
+        {isToggle && (
+          <S.MateToggleMenuLayout ref={toggleRef}>
+            <S.MateToggleMenuContainer
+              initial={{ opacity: 0, height: 0 }}
+              animate={{ opacity: 1, height: "100%" }}
+              exit={{ opacity: 0, height: 0 }}
+            >
+              <S.MateToggleMenuItem>
+                <EditPencil className="w-6 h-6" />
+                수정
+              </S.MateToggleMenuItem>
+              <Divider />
+              <S.MateToggleMenuItem>
+                <Trash className="w-6 h-6" /> 삭제
+              </S.MateToggleMenuItem>
+            </S.MateToggleMenuContainer>
+          </S.MateToggleMenuLayout>
+        )}
+      </AnimatePresence>
     </>
   );
 };
