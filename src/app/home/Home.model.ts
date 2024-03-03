598e0c6ae7fb2174da68e44c659120276d8b2fb9
seunghyun <tmdgus7820@naver.com>
2024-03-03 22:59:43 +0900

    ♻️refactor: model에 있던 api 호출 함수들 controller로 로직 변경

--- src/app/home/Home.model.ts
+++ src/app/home/Home.model.ts
@@ -1,42 +1,12 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import useUserLocationStore from "@/stores/useUserLocationStore";
-import useGetCurrentPosition from "./hook/useGetCurrentPosition";
-import useGetWeatherForecast from "./hook/useGetweatherForecast";
-import useGetAirQuality from "./hook/useGetAirQuality";
-import convertLatLonToTM from "./utils/convertLatLonToTM";
+import { useState } from "react";
 
 const useHomeModel = () => {
   const [isNotification] = useState<null | boolean>(null);
 
-  const { setUserLocation } = useUserLocationStore();
-  const { location } = useGetCurrentPosition();
-  const { getWeatherForecast, temperature, weather, precipitation } = useGetWeatherForecast();
-  const { pm10, address, getNearbyStation } = useGetAirQuality();
-
-  useEffect(() => {
-    if (location.lat > 0 && location.lng > 0) {
-      const coords = convertLatLonToTM(location.lat, location.lng); // 위도와 경도를 TM 좌표계로 변환
-
-      const tmX = coords.x;
-      const tmY = coords.y;
-
-      if (tmX && tmY && tmX > 0 && tmY > 0) {
-        getNearbyStation(tmX, tmY);
-        getWeatherForecast(location);
-        setUserLocation(location);
-      }
-    }
-  }, [location]);
-
   return {
     isNotification,
-    temperature,
-    weather,
-    precipitation,
-    pm10,
-    address,
   };
 };
 
---
+++ src/app/home/hook/index.ts
@@ -0,0 +1,2 @@
+export { default as useGetAirQuality } from "./useGetAirQuality";
+export { default as useGetWeatherForecast } from "./useGetWeatherForecast";
