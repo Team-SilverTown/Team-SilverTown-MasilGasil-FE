b3b0aa0a05016c227ec47145b7dc8ccf1846688d
Zoody <reopard506@gmail.com>
2024-03-04 13:09:15 +0900

    ♻️refactor: Tab 컴포넌트 리펙토링
    - Tab Component props 를 통해 지정된 Tab Component 이외의 컴포넌트를 받을 수 있도록 구현

---
+++ src/app/explore/components/TabItem/TabItem.styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { FONT_SIZE, FONT_WEIGHT } from "@/styles/theme";
+
+export const Tab = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1 1 0%;
+  height: 100%;
+  padding: 0.5rem 0.75rem;
+  border-radius: 9999px;
+`;
+
+export const TabContent = styled.span<any>`
+  position: relative;
+  z-index: 10;
+
+  font-weight: ${FONT_WEIGHT.SEMIBOLD};
+  font-size: ${FONT_SIZE.MEDIUM};
+  color: ${(props) =>
+    props.$isFocused ? props.theme.text_secondary_color : props.theme.transparent_50};
+`;
+
+export const TabActive = styled(motion.div)`
+  position: absolute;
+  inset: 0;
+  border-radius: 9999px;
+  background-color: ${(props) => props.theme.green_500};
+`;
---
+++ src/app/explore/components/TabItem/TabItem.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+import * as S from "./TabItem.styles";
+
+interface TabItemProps {
+  item: string;
+  index: number;
+  focusedTab: number;
+}
+
+const TabItem = ({ item, index, focusedTab }: TabItemProps) => {
+  return (
+    <S.Tab key={index}>
+      <S.TabContent
+        $isFocused={focusedTab === index}
+        className={"z-10 transition-colors duration-500"}
+      >
+        {item}
+      </S.TabContent>
+      {focusedTab == index && (
+        <motion.div
+          layoutId="active-fill"
+          className="absolute inset-0 rounded-full bg-green-500"
+        />
+      )}
+    </S.Tab>
+  );
+};
+
+export default TabItem;
