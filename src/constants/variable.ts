eb1a8bf3ed44fd284d63eb7ce9bd0b0fe20a6e63
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-03-04 15:33:37 +0900

    ♻️refactor: 성별 타입 별도로 관리

--- src/constants/variable.ts
+++ src/constants/variable.ts
@@ -1,3 +1,5 @@
+import { SexOption } from "@/types/OriginDataType/SexOption";
+
 export const INTENSITY_OPTIONS = [
   { label: "좌식", value: "SUPER_LOW" },
   { label: "가벼운 활동(가벼운 운동/스포츠 3-5일/주)", value: "LOW" },
@@ -6,12 +8,7 @@ export const INTENSITY_OPTIONS = [
   { label: "매우 활독적인 경우 (매우 힘든 운동/스포츠 및 육체 노동)", value: "SUPER_HIGH" },
 ];
 
-export interface SexOptionProps {
-  label: string;
-  value: "male" | "female";
-}
-
-export const SEX_OPTIONS: SexOptionProps[] = [
+export const SEX_OPTIONS: SexOption[] = [
   { label: "남성", value: "male" },
   { label: "여성", value: "female" },
 ];
