70b9a9b92c1cb63af8c5a1c5e49e810f2a811ec0
seunghyun <tmdgus7820@naver.com>
2024-03-04 20:23:19 +0900

    🐛fix: 1시간 이상이면서 0분일 때는 시간만 나오는 로직 추가

--- src/utils/convertSeconds.ts
+++ src/utils/convertSeconds.ts
@@ -3,6 +3,7 @@
  * @param sec 1초 단위로 전달되는 매개변수 입니다.
  * @returns 산책 시간이 1분 미만이라면 1분을 보여줍니다.
  *          산책 시간이 1시간 미만이라면 분만 보여줍니다.
+ *          산책 시간이 1시간 이상이지만 0분이라면 시간만 보여줍니다.
  *          산책 시간이 1시간 이상이라면 시간, 분을 같이 보여줍니다.
  */
 
@@ -12,5 +13,6 @@ export const convertSeconds = (sec: number): string => {
   minutes = minutes % 60;
   if (sec < 60) return `1분`;
   if (hours === 0) return `${minutes}분`;
+  if (minutes === 0) return `${hours}시간`;
   return `${hours}시간 ${minutes}분`;
 };
