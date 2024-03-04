fd241f818794e9ed80f01525b27f25e73b07adf0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-03-04 19:00:05 +0900

    [Feat] Mate Detail 페이지 레이아웃 구현 (#135)
    * ✨feat: MateDetail 페이지 생성
    
    * ✨feat: Divider 컴포넌트 구현
    
    * ✨feat: MateDetail layout 구현
    
    * ✨feat: Mate Info 구현
    
    * 🎨style: tailwind config theme 수정
    
    * ✨feat: MateEvaluation 구현
    
    * ✨feat: MateMembers 구현 및 스타일 전역화
    
    * 🐛fix: tailwind 색상 적용 오류 수정
    
    * ✨feat: TopNavigator GoBackButton 추가
    
    * [Feat] MateDetail 참가자, 유저 평가 구역 구현 (#137)
    
    * ✨feat: 임시 더미 데이터 및 더미 타입 생성
    
    * 🗑️remove: 임시 yarn lock 제거
    
    * ✨feat: Members 구현
    
    * ✨feat: 임시 데이터 및 타입 추가
    
    * ✨feat: MateEvaluation 구현 및 스타일링
    
    * ♻️refactor: MateEvaluation 개선
    
    * ♻️refactor: Avatar 클릭 이벤트 개선 및 적용
    
    * Revert "🗑️remove: 임시 yarn lock 제거"
    
    This reverts commit 4e1090959e527470c676ce681bb01e80adcab3f9.
    
    * ♻️refactor: 스타일 개선 및 코드 개선
    
    * [Feat] MateDetail 메이트 정보란 구현 (#139)
    
    * 🐛fix: avatar common cursor 관련 문제 수정
    
    * ✨feat: MateToggleMenu 생성
    
    * ✨feat: MateInfo의 UserInfo 구역 생성
    
    * ✨feat: Dummy 임시 데이터 생성
    
    * ✨feat: useToggle 구현
    
    * 🎨style: MateToggleMenu 스타일 구현
    
    * ✨feat: MateToggleMenu 동작 및 애니메이션 구현
    
    * ✨feat: 버튼 클릭 이벤트 부착 및 스타일 개선
    
    * ✨feat: MateInfo 리스트 구현
    
    * 🐛fix: 애니메이션 동작 위치 문제 수정
    
    * ✨feat: MateInfo 의 버튼 리스트 구현
    
    * remove: 테스트용 코드 제거 및 스타일 개선
    
    * ♻️refactor: 스타일 관련 리뷰 사항수정

--- src/app/mate/[id]/page.tsx
+++ src/app/mate/[id]/page.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import MateDetail from "./MateDetail";
+import { MatePost } from "./MateDetail.types";
 
-const Mate = () => {
-  return <div>Mate</div>;
+interface MateProps {
+  params: { id: string };
+}
+
+const Mate = ({ params }: MateProps) => {
+  const { id } = params;
+
+  return (
+    <MateDetail
+      postId={id}
+      matePost={DUMMY_DATA}
+      authorEvaluation={DUMMY_USER_EVALUATION}
+    />
+  );
 };
 
 export default Mate;
+
+const DUMMY_DATA: MatePost = {
+  authorNickname: "재영",
+  authorThumbnailUrl: "",
+
+  content: `길게길게 써야할것 같은데 뭐라고 써야할지 모르겠어요! 일단 이건 더미 데이터니까 아무말이나 미친듯이 쓰고 있는데 혹시 저희 글자 제한이 존재할까요? 근데 content니까 글자 제한을 두는것도 좀 그럴려나요? 그리고 이걸 엄청 길게 쓰는 사람이 있을까요? 태그도 만들어야할까요? 고민할게 많네요?? 근데 방학이 방학같지 않네요.. 할것도 없고 후... 우리팀이 압도적으로 UI가 이쁠수도 있을것같아요 근데 이러고 최종 발표회때 우와.. 하면서 다른 팀걸 지켜보고있겠죠? 이정도로 길게쓰면 될까요?`,
+
+  // 일단 임시로 스트링으로 직접 날짜 표시 -  추후 변환 함수 생성 예장
+  mateTime: "2024-3-10 오후 3:40",
+  // location에 대한 부분도 상의 필요
+  mateLocation: "오산천",
+
+  recruitedUser: 5,
+  recruitingUser: 10,
+
+  members: [
+    { user_id: "22", state: "invited", thumbnailUrl: "", nickname: "기술 브레인 원주" },
+    { user_id: "33", state: "invited", thumbnailUrl: "", nickname: "멘탈 브레이커 송희" },
+    { user_id: "44", state: "invited", thumbnailUrl: "", nickname: "디자인 천재 재웅" },
+    { user_id: "55", state: "invited", thumbnailUrl: "", nickname: "CSS의 황제 승현" },
+  ],
+};
+
+const DUMMY_USER_EVALUATION = {
+  kind: 9172613,
+  time: 99214718,
+  bad: 0,
+};
