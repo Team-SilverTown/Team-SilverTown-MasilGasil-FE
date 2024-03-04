50666eba7563b299ac0f52ccf4cd6bedd3237d1d
하송희 <99heesong@naver.com>
2024-03-05 08:23:49 +0900

    🎨style: 메이트 모집하기 폼 스타일링

---
+++ src/app/mate/create/MateCreate.styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const MateCreateContainer = styled.div``;
+
+export const Title = styled.strong`
+  display: block;
+  font-size: 2rem;
+  margin-bottom: 1.4rem;
+`;
+
+export const TitleSection = styled.section`
+  margin-top: 3rem;
+`;
+
+export const ContentSection = styled.section`
+  margin-top: 3rem;
+`;
+
+export const LocationSection = styled.section`
+  margin-top: 3rem;
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+  margin-top: 3rem;
+  gap: 2rem;
+`;
+
+export const DateSection = styled.section`
+  margin-top: 3rem;
+`;
+
+export const TimeSection = styled.section`
+  flex: 3;
+`;
+
+export const PersonnelSection = styled.section`
+  flex: 2;
+`;
