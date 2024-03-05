df12f6082835efa2b3da785a783366497b8253a4
seunghyun <tmdgus7820@naver.com>
2024-03-05 10:07:48 +0900

    ✨feat: calculateAge 함수 추가 - year-month-day 형식의 매개변수를 전달받아서 만 나이를 반환해 주는 함수 추가

---
+++ src/utils/calculateAge.ts
@@ -0,0 +1,13 @@
+export const calculateAge = (birthDate: string): number => {
+  const birthday = new Date(birthDate);
+  const today = new Date();
+
+  let age = today.getFullYear() - birthday.getFullYear();
+  const monthDifference = today.getMonth() - birthday.getMonth();
+  birthday;
+  if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthday.getDate())) {
+    age--;
+  }
+
+  return age;
+};
