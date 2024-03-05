10aa4f852cebc06dbdbe71832b73720ec50feb39
Zoody <reopard506@gmail.com>
2024-03-05 12:42:24 +0900

    ♻️refactor: API 핸들러 CRUD 메소드 추가

--- src/lib/api/clientRootAPI.ts
+++ src/lib/api/clientRootAPI.ts
@@ -41,7 +41,49 @@ export const POST = async <T>({
     const response = await API.post<T>(`/api${endPoint}`, data, config);
     return response;
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    return undefined;
+  }
+};
+
+export const PUT = async <T>({
+  endPoint,
+  data,
+  config,
+  auth,
+}: {
+  endPoint: string;
+  data?: unknown;
+  config?: AxiosRequestConfig;
+  auth?: boolean;
+}) => {
+  const API = getRootAPI(auth);
+
+  try {
+    const response = await API.put<T>(`/api${endPoint}`, data, config);
+    return response;
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+};
+
+export const DELETE = async <T>({
+  endPoint,
+  config,
+  auth,
+}: {
+  endPoint: string;
+  config?: AxiosRequestConfig;
+  auth?: boolean;
+}) => {
+  const API = getRootAPI(auth);
+
+  try {
+    const response = await API.delete<T>(`/api${endPoint}`, config);
+    return response;
+  } catch (error) {
+    console.error(error);
     return undefined;
   }
 };
