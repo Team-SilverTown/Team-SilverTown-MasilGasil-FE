48b9e282af0b9cb437c49cc2850a8906746b9a6f
하송희 <99heesong@naver.com>
2024-03-05 21:25:21 +0900

    🎨style: CalendarDatePicker 입력부분 스타일링

--- src/app/mate/create/components/CalendarDatePicker/CalendarDatePicker.tsx
+++ src/app/mate/create/components/CalendarDatePicker/CalendarDatePicker.tsx
@@ -4,16 +4,30 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { ko } from "date-fns/locale";
 
+import * as S from "./CalendarDatePicker.styles";
+
 const CalendarDatePicker = () => {
-  const [startDate, setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(null);
+
+  const CustomInput = ({ value, onClick, startDate }) => (
+    <S.CustomInput
+      onClick={onClick}
+      isSelected={startDate !== null}
+      readOnly
+    >
+      {value || "모임 날짜를 선택하세요"}
+    </S.CustomInput>
+  );
 
   return (
+    // TODO: width=100%임에도 화면 가득 차지 않는 문제 해결
     <DatePicker
       locale={ko}
       dateFormat="yyyy년 MM월 dd일"
       selected={startDate}
       minDate={new Date()}
       onChange={(date: Date) => setStartDate(date)}
+      customInput={<CustomInput startDate={startDate} />}
     />
   );
 };
