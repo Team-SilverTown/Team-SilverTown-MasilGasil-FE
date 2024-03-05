174d7926e12c56ec99a9428b43eb9d214c563801
seunghyun <tmdgus7820@naver.com>
2024-03-06 00:12:17 +0900

    ♻️refactor: WalkList 컴포넌트 수정 (Router 방식을 Link 방식으로 변경) - useRouter hook을 제거함으로써 서버 컴포넌트로 사용할 수 있음

--- src/app/home/components/WalkList/WalkList.tsx
+++ src/app/home/components/WalkList/WalkList.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { MasilsListType, PostsListType } from "../../Home.types";
 import { LogDetailCard } from "@/components";
 import * as S from "./WalkList.styles";
+import Link from "next/link";
 
 interface WalkListProps {
   walkList: MasilsListType[] | PostsListType[];
@@ -11,18 +14,23 @@ const WalkList = ({ walkList }: WalkListProps) => {
     <S.WalkListContainer>
       {walkList.map((list) => (
         <li key={list.id}>
-          <LogDetailCard
-            title="오산시"
-            content="내용"
-            thumbnailURL="https://github.com/SeungHyune/first-script/assets/114329713/34992788-3558-40cd-940c-81f7f2b94551"
-            distance="1.5km"
-            totalTime="30분"
-            likeCount={1000}
-            isLiked={true}
-            isLikeLayout={true}
-            isSettingLayout={false}
-            onDetailClick={() => {}}
-          />
+          <Link
+            href={`/posts/${list.id}`}
+            style={{ display: "flex" }}
+          >
+            <LogDetailCard
+              title="오산시"
+              content="내용"
+              thumbnailURL="https://github.com/SeungHyune/first-script/assets/114329713/34992788-3558-40cd-940c-81f7f2b94551"
+              distance="1.5km"
+              totalTime="30분"
+              likeCount={1000}
+              isLiked={true}
+              isLikeLayout={true}
+              isSettingLayout={true}
+              onDetailClick={() => {}}
+            />
+          </Link>
         </li>
       ))}
       {walkList.length === 0 && (
