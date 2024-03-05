e8a7198189ef80d0a2c2a4c34565a9e23133ae0b
seunghyun <tmdgus7820@naver.com>
2024-03-05 14:26:09 +0900

    ♻️refactor: view 레이아웃에 TopNav 추가 했습니다. - Top Nav에는 뒤로가기 버튼과 설정 페이지 이동 버튼이 존재합니다.

--- src/app/user/[id]/Mypage.controller.tsx
+++ src/app/user/[id]/Mypage.controller.tsx
@@ -1,11 +1,33 @@
-"use client";
-
+import Link from "next/link";
+import { RECORDLIST_DUMMY_DATA, USERINFO_DUMMY_DATA } from "./Mypage.constants";
 import MypageView from "./MyPage.view";
-import useMypageModel from "./Mypage.model";
+import { TopNavigator } from "@/components/navigators/TopNavigator";
+import { GoBackButton } from "@/components/navigators/TopNavigator/components";
+import Setting from "@/components/icons/Setting";
 
 const MypageController = () => {
-  const { boardList } = useMypageModel();
-  return <MypageView boardList={boardList} />;
+  return (
+    <>
+      <TopNavigator
+        leftChildren={<GoBackButton />}
+        title="프로필"
+        rightChildren={
+          <Link
+            href="/user/:id/setting"
+            title="설정 페이지 바로가기"
+            style={{ marginRight: "1.5rem" }}
+          >
+            <Setting />
+          </Link>
+        }
+      />
+      <MypageView
+        boardList={RECORDLIST_DUMMY_DATA}
+        userInfo={USERINFO_DUMMY_DATA}
+      />
+      ;
+    </>
+  );
 };
 
 export default MypageController;
