e03695ef1102c1e8a4be8942f5a9f3ba194d570e
하송희 <99heesong@naver.com>
2024-03-05 14:56:43 +0900

    ✨feat: CalendarDatePicker 한국어로 날짜 표기, 현재 날짜 이전으로는 선택 못 하게 설정

--- src/app/mate/create/components/CalendarDatePicker/CalendarDatePicker.tsx
+++ src/app/mate/create/components/CalendarDatePicker/CalendarDatePicker.tsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
+import { ko } from "date-fns/locale";
 
 const CalendarDatePicker = () => {
   const [startDate, setStartDate] = useState(new Date());
 
   return (
     <DatePicker
-      dateFormat="yyyy/MM/dd"
+      locale={ko}
+      dateFormat="yyyy년 MM월 dd일"
       selected={startDate}
+      minDate={new Date()}
       onChange={(date: Date) => setStartDate(date)}
     />
   );
