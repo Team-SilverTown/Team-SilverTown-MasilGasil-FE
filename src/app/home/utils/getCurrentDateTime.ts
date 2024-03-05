4bc0114727e7fb2881048ee5d11537a446ffb2df
seunghyun <tmdgus7820@naver.com>
2024-03-06 00:32:02 +0900

    ♻️refactor: getCurrentDateTime 함수 수정 - 날씨 API가 30분 단위마다 갱신되어 00시 30분 이전 시간의 경우 전날 23시 30분 날씨 정보를 참조하도록 수정

--- src/app/home/utils/getCurrentDateTime.ts
+++ src/app/home/utils/getCurrentDateTime.ts
@@ -3,20 +3,23 @@ const getCurrentDateTime = () => {
 
   const year = now.getFullYear();
   const month = (now.getMonth() + 1).toString().padStart(2, "0");
-  const day = now.getDate().toString().padStart(2, "0");
+  let day = now.getDate().toString().padStart(2, "0");
   let hours: number | string = now.getHours();
   let minutes = "30";
 
-  if (now.getMinutes() < 30) {
+  if (now.getMinutes() <= 30) {
     hours -= 1;
   }
 
   if (hours < 0) {
-    hours = "00";
-    minutes = "00";
+    hours = "23";
+    day = (now.getDate() - 1).toString().padStart(2, "0");
   } else {
     hours = hours.toString().padStart(2, "0");
   }
+
+  console.log(year, month, day);
+  console.log(hours, minutes);
 
   return {
     date: `${year}${month}${day}`,
