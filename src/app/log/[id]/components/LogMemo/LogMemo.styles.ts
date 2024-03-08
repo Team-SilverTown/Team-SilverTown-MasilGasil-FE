5f21d1d5fc007a1310cfa61177b4523c1bf066a2
seunghyun <tmdgus7820@naver.com>
2024-03-08 12:03:08 +0900

    ✨feat: LogMemo 컴포넌트 추가 - Memo 탭의 내용을 보여준다. (산책 완료 후 등록한 산책 정보와 내용을 보여준다.)

---
+++ src/app/log/[id]/components/LogMemo/LogMemo.styles.ts
@@ -0,0 +1,44 @@
+import { FONT_SIZE, FONT_WEIGHT } from "@/styles/theme";
+import styled from "styled-components";
+
+export const LogMemoLocation = styled.article`
+  font-weight: ${FONT_WEIGHT.MEDIUM};
+`;
+
+export const LogMemoWalkInfo = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 2.5rem;
+  margin-bottom: 2.8rem;
+
+  li {
+    flex: 1;
+    text-align: center;
+
+    strong {
+      padding: 0.5rem 1rem;
+      font-size: ${FONT_SIZE.MINI};
+      font-weight: ${FONT_WEIGHT.REGULAR};
+      color: ${(props) => props.theme.white};
+      background-color: ${(props) => props.theme.green_500};
+      border-radius: 2rem;
+    }
+
+    span {
+      margin-top: 1.5rem;
+      display: block;
+    }
+  }
+`;
+
+export const LogMemoContent = styled.article`
+  font-size: ${FONT_SIZE.MINI};
+  line-height: 1.6;
+`;
+
+export const LogMemoDate = styled.article`
+  margin-top: 2rem;
+  text-align: right;
+  font-size: ${FONT_SIZE.MINI};
+  color: ${(props) => props.theme.gray_300};
+`;
---
+++ src/app/log/[id]/components/LogMemo/LogMemo.tsx
@@ -0,0 +1,35 @@
+import * as S from "./LogMemo.styles";
+
+interface LogMemoProps {
+  distance?: number;
+  totalTime?: number;
+}
+
+const LogMemo = ({ distance, totalTime }: LogMemoProps) => {
+  return (
+    <>
+      <S.LogMemoLocation>경남 진주시 호탄동</S.LogMemoLocation>
+      <S.LogMemoWalkInfo>
+        <li>
+          <strong>산책 거리</strong>
+          <span>1.1km</span>
+        </li>
+        <li>
+          <strong>소요 시간</strong>
+          <span>30분</span>
+        </li>
+        <li>
+          <strong>칼로리 소모</strong>
+          <span>200kcal</span>
+        </li>
+      </S.LogMemoWalkInfo>
+      <S.LogMemoContent>
+        진주시에서 가장 큰 강이 남강변을 따라 걷는 산책로에요. 주변을 걷다보면 수달을 만날 수도
+        있답니다. 행운의 수달을 만나면 소원을 빌어보아요! #데이트코스 #가볍게걷기좋은 #사계절 #수달
+      </S.LogMemoContent>
+      <S.LogMemoDate>2024-02-25 오후 11:11</S.LogMemoDate>
+    </>
+  );
+};
+
+export default LogMemo;
