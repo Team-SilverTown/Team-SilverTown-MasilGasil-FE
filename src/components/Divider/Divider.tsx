e9963eb1fbafd514f364966899f8dd74c3ac1f18
joseunghyeon <tmdgus7820@naver.com>
2024-03-08 17:02:06 +0900

    🎨style: Divider 컴포넌트 수정 - props로 style 수정할 수 있도록 수정

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,12 +1,20 @@
+import { CSSProperties } from "react";
+
 interface DividerProps {
   isColumn?: boolean;
+  style?: CSSProperties;
 }
 
-const Divider = ({ isColumn = false }: DividerProps) => {
+const Divider = ({ isColumn = false, style }: DividerProps) => {
   const columnStyle = "w-1 min-w-1 h-full";
   const rowStyle = "w-full min-h-1 h-1";
 
-  return <div className={`${isColumn ? columnStyle : rowStyle} rounded bg-[#E3E3E3]`} />;
+  return (
+    <div
+      style={style}
+      className={`${isColumn ? columnStyle : rowStyle} rounded bg-[#E3E3E3]`}
+    />
+  );
 };
 
 export default Divider;
