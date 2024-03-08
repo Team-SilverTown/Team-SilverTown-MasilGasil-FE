62aaf028a211d476190d732a24d6fea1b22be93e
seunghyun <tmdgus7820@naver.com>
2024-03-08 11:59:08 +0900

    🎨style: Log 페이지 TopNav 아이콘 추가 (KebabMenu Icon)

---
+++ src/app/log/[id]/components/LogKebabMenu/LogKebabMenu.tsx
@@ -0,0 +1,11 @@
+import { KebabMenu } from "@/components/icons";
+
+const LogKebabMenu = () => {
+  return (
+    <div style={{ marginRight: "1.5rem", cursor: "pointer" }}>
+      <KebabMenu style={{ width: "0.8rem", height: "2rem" }} />
+    </div>
+  );
+};
+
+export default LogKebabMenu;
