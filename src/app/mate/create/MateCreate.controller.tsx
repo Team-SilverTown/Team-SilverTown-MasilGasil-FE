3d8797d47e8db5175b389bb2fe70da40dd040e91
하송희 <99heesong@naver.com>
2024-03-08 12:12:03 +0900

    ✨feat: fromData에 시간, 날짜, 인원수 전달

--- src/app/mate/create/MateCreate.controller.tsx
+++ src/app/mate/create/MateCreate.controller.tsx
@@ -26,7 +26,22 @@ const MateCreateController = () => {
   });
 
   const onValid = (data: MateCreateProps) => {
-    console.log("Form Data:", data);
+    const date = startDate
+      ? new Date(startDate.getTime() - startDate.getTimezoneOffset() * 60000)
+          .toISOString()
+          .substring(0, 10)
+      : ""; // startDate는 00시 기준이니까 한국에 맞게 수정
+    const time = startTime ? startTime.toISOString().substring(11, 19) : "";
+    const gatheringAt = `${date}T${time}.000Z`;
+
+    const completeData = {
+      ...data,
+      startDate,
+      startTime,
+      gatheringAt,
+      selectedPersonnel,
+    };
+    console.log("Complete Form Data:", completeData);
   };
 
   const onInvalid = (errors: any) => {
