706d9d4c63426fd700f32ce93cdedc1c2d09b283
seunghyun <tmdgus7820@naver.com>
2024-03-10 17:21:46 +0900

    ✨feat: calculateWalkingCalories 칼로리 계산 함수 추가 - 유저의 신체 정보가 있는 경우 산책 거리에 따른 소모 칼로리를 반환해 주는 함수입니다. - 기존의 hook을 제거하고 함수로 새롭게 정의했습니다.

--- src/utils/calculateWalkingCalories.ts
+++ src/utils/calculateWalkingCalories.ts
@@ -1,12 +1,50 @@
-const calculateWalkingCalories = (
-  sex: "MALE" | "FEMALE",
-  age: number,
-  height: number,
-  weight: number,
-  exerciseIntensity: "SUPER_LOW" | "LOW" | "MIDDLE" | "HIGH" | "SUPER_HIGH",
-  distance: number,
-  walkingMET: number = 3.3,
-): number => {
+import { UserInfoType } from "@/app/user/[id]/MyPage.types";
+import { calculateAge } from ".";
+
+interface calculateWalkingCaloriesProps {
+  userInfo: UserInfoType;
+  distance: number;
+  walkingMET?: number;
+}
+
+interface calculateWalkingCaloriesResponse {
+  isUserInfoCheck: boolean;
+  calories: null | number;
+}
+
+/**
+ * 산책을 통해 소모된 칼로리를 계산합니다. 유저의 신체 정보와 산책한 거리를 기반으로 계산하며,
+ * 유저의 정보가 충분하지 않은 경우 유효하지 않음을 반환합니다.
+ * @param userInfo params.userInfo 유저의 신체 정보를 포함하는 객체입니다. 이 객체는 성별(sex), 생년월일(birthDate),
+ *        키(height), 몸무게(weight), 운동 강도(exerciseIntensity)를 속성으로 가집니다.
+ * @param distance params.distance 유저가 산책한 거리(m)입니다.
+ * @param walkingMET [params.walkingMET=3.3] 걷기 운동의 대사당량(MET) 값으로, 기본값은 3.3입니다.
+ *        이 값은 걷기 운동의 강도를 나타냅니다.
+ * @returns {Object} 계산 결과를 포함하는 객체입니다. 이 객체는 유저 정보의 유효성(isUserInfoCheck)과
+ *          소모된 칼로리(calories)를 속성으로 가집니다. 유저 정보가 유효하지 않은 경우,
+ *          isUserInfoCheck는 false이고 calories는 null입니다. 유저 정보가 유효한 경우,
+ *          isUserInfoCheck는 true이고 calories는 소모된 칼로리의 수치(반올림된 정수)입니다.
+ */
+
+const calculateWalkingCalories = ({
+  userInfo,
+  distance,
+  walkingMET = 3.3,
+}: calculateWalkingCaloriesProps): calculateWalkingCaloriesResponse => {
+  const { sex, birthDate, height, weight, exerciseIntensity } = userInfo;
+
+  let age: number | null = null;
+  if (birthDate) {
+    age = calculateAge(birthDate);
+  }
+
+  if (!sex || !age || !height || !weight || !exerciseIntensity) {
+    return {
+      isUserInfoCheck: false,
+      calories: null,
+    };
+  }
+
   // 기초 대사량(BMR) 계산
   let BMR: number = 0;
   if (sex === "MALE") {
@@ -44,7 +82,10 @@ const calculateWalkingCalories = (
   // 걷기로 인한 칼로리 소모량 계산
   const calories = ((adjustedBMR * walkingMET) / 24) * walkingTime;
 
-  return Math.round(calories);
+  return {
+    isUserInfoCheck: true,
+    calories: Math.round(calories),
+  };
 };
 
 export default calculateWalkingCalories;
