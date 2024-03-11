b75e148c14a0626f7b87321647757aaae0973f47
하송희 <99heesong@naver.com>
2024-03-11 09:54:26 +0900

    🐛fix: git pull origin/feature/MateCreateForm/#116 충돌 해결

--- src/app/user/[id]/setting/UserSetting.types.ts
+++
@@ -1,3 +0,0 @@
-export type SwitchType = "NOTIFICATION" | "ACCOUNT";
-
-export type OnClickSettings = (type?: SwitchType) => void;
--- src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as calculateAge } from "./calculateAge";
-export { default as calculateWalkingCalories } from "./calculateWalkingCalories";
-export { default as convertDateToYearMonthDay } from "./convertDateToYearMonthDay";
-export { default as convertMeter } from "./convertMeter";
-export { default as convertSeconds } from "./convertSeconds";
