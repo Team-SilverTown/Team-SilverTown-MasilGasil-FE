65b0894ab4002db3fe7613705bf28cb60bbaa80e
seunghyun <tmdgus7820@naver.com>
2024-03-11 23:29:41 +0900

    ✨feat: PostKebabMenu 컴포넌트 분리

---
+++ src/app/post/[id]/components/PostKebabMenu/PostKebabMenu.tsx
@@ -0,0 +1,11 @@
+import { MateDropDownMenu } from "@/app/mate/[id]/components/MateInfo/components";
+
+const PostKebabMenu = () => {
+  return (
+    <div style={{ marginRight: "1.5rem", cursor: "pointer" }}>
+      <MateDropDownMenu />
+    </div>
+  );
+};
+
+export default PostKebabMenu;
