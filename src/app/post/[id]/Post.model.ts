2d826fca3239519a8df66e931bcc6bc29427d7a5
seunghyun <tmdgus7820@naver.com>
2024-03-12 03:25:51 +0900

    ✨feat: PostView 페이지 model에 API 데이터 연결 - API를 통해 받아온 path 값으로 path의 중간 지점을 변환 - postData를 props로 전달 - useMeStore를 통해 내 추가 정보를 가지고 오도록 설정

--- src/app/post/[id]/Post.model.ts
+++ src/app/post/[id]/Post.model.ts
@@ -1,17 +1,47 @@
 "use client";
 
+import { useEffect, useMemo, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+import calculatePathCenter from "@/lib/utils/calculatePathCenter";
+
+import { getPostDetail } from "@/lib/api/Post/client";
+import { GET_KEY } from "@/lib/api/queryKeys";
+
 import { GeoPosition } from "@/types/OriginDataType";
 import { PostTabType } from "./Post.types";
-import { useState } from "react";
+import useMeStore from "@/stores/useMeStore";
+import { MATE_DUMMY_DATA } from "./Post.constants";
 
-interface usePostModelProps {
-  baseLocation: GeoPosition;
+interface usePostModelProp {
+  postId: string;
 }
 
-const usePostModel = ({ baseLocation }: usePostModelProps) => {
+const usePostModel = ({ postId }: usePostModelProp) => {
   const [tabIndex, setTabIndex] = useState(PostTabType.Memo);
   const [currentPinIndex, setCurrentPinIndex] = useState(0);
-  const [mapCenter, setMapCenter] = useState<GeoPosition>(baseLocation);
+  const [mapCenter, setMapCenter] = useState<GeoPosition>({
+    lat: 0,
+    lng: 0,
+  });
+  const { data: postData } = useQuery({
+    queryKey: [GET_KEY.GET_LOG],
+    queryFn: () => getPostDetail({ id: postId }),
+  });
+
+  const { sex, birthDate, weight, height, exerciseIntensity } = useMeStore();
+  const userInfo = { sex, birthDate, weight, height, exerciseIntensity };
+
+  const baseLocation = useMemo(() => {
+    if (postData) {
+      return calculatePathCenter(postData.path);
+    }
+    return { lat: 0, lng: 0 };
+  }, [postData]);
+
+  useEffect(() => {
+    setMapCenter(baseLocation);
+  }, [baseLocation]);
 
   return {
     tabIndex,
@@ -20,6 +50,10 @@ const usePostModel = ({ baseLocation }: usePostModelProps) => {
     setCurrentPinIndex,
     mapCenter,
     setMapCenter,
+    postData,
+    baseLocation,
+    userInfo,
+    mateData: MATE_DUMMY_DATA,
   };
 };
 
