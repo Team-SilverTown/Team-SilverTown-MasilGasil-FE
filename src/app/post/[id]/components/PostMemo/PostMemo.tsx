8f665c015d101f3519569d2dd78438940df14a4d
seunghyun <tmdgus7820@naver.com>
2024-03-12 03:34:43 +0900

    ♻️refactor: PostMemo 컴포넌트 수정 - postData에 따른 실 데이터로 수정

--- src/app/post/[id]/components/PostMemo/PostMemo.tsx
+++ src/app/post/[id]/components/PostMemo/PostMemo.tsx
@@ -1,28 +1,28 @@
-import { Location } from "@/components/icons";
 import { calculateWalkingCalories, convertMeter, convertSeconds } from "@/utils";
-import { MeResponse } from "@/types/Response";
-import * as S from "./PostMemo.styles";
+import { PostDetailResponse } from "@/types/Response/Post";
 import Avatar from "@/components/Avatar/Avatar";
+import { Location } from "@/components/icons";
+import { UserDummyType } from "../../Post.types";
+import * as S from "./PostMemo.styles";
 
 interface PostMemoProps {
-  distance: number;
-  totalTime: number;
-  userInfo: MeResponse;
-  address: string;
+  userInfo: UserDummyType;
+  postData: PostDetailResponse;
 }
 
-const PostMemo = ({ distance, totalTime, userInfo, address }: PostMemoProps) => {
+const PostMemo = ({ userInfo, postData }: PostMemoProps) => {
+  const { distance, totalTime, content, title, depth1, depth2 } = postData;
   const { isUserInfoCheck, calories } = calculateWalkingCalories({ userInfo, distance });
 
   return (
     <>
-      <S.PostMemoTitle>타이틀은 30자 이하</S.PostMemoTitle>
+      <S.PostMemoTitle>{title}</S.PostMemoTitle>
       <S.PostMemoInfo>
         <S.PostMemoProfile>
           <Avatar /> <span>닉네임</span>
         </S.PostMemoProfile>
         <S.PostMemoLocation>
-          <Location /> {address}
+          <Location /> {depth1} {depth2}
         </S.PostMemoLocation>
       </S.PostMemoInfo>
       <S.PostMemoWalkInfo>
@@ -41,10 +41,7 @@ const PostMemo = ({ distance, totalTime, userInfo, address }: PostMemoProps) => 
           </li>
         )}
       </S.PostMemoWalkInfo>
-      <S.PostMemoContent>
-        진주시에서 가장 큰 강이 남강변을 따라 걷는 산책로에요. 주변을 걷다보면 수달을 만날 수도
-        있답니다. 행운의 수달을 만나면 소원을 빌어보아요! #데이트코스 #가볍게걷기좋은 #사계절 #수달
-      </S.PostMemoContent>
+      <S.PostMemoContent>{content}</S.PostMemoContent>
     </>
   );
 };
