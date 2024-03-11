db2acb693f8440d6507a9afc177d8a76887f55d2
Zoody <reopard506@gmail.com>
2024-03-11 17:19:02 +0900

    🔥hotfix: vercel cors 설정

--- next.config.mjs
+++ next.config.mjs
@@ -57,6 +57,23 @@ export default withPWA(
           },
         ];
       },
+      async headers() {
+        return [
+          {
+            source: "/call/:path*",
+            headers: [
+              { key: "Access-Control-Allow-Credentials", value: "true" },
+              { key: "Access-Control-Allow-Origin", value: "*" },
+              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+              {
+                key: "Access-Control-Allow-Headers",
+                value:
+                  "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              },
+            ],
+          },
+        ];
+      },
 
       images: { domains: ["masilgasil-s3.s3.amazonaws.com", "github.com"] },
     }),
