deaa5b0a42a301f6edd607d6bda3abcccb7c3ef0
seunghyun <tmdgus7820@naver.com>
2024-03-12 15:35:43 +0900

    ✨feat: LogModel에 API 로직 연동 - useQuery로 masilData 가지고 오는 로직 추가 - 초기 baseLocation 설정

--- src/app/log/[id]/Log.model.ts
+++ src/app/log/[id]/Log.model.ts
@@ -1,25 +1,56 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+import useMeStore from "@/stores/useMeStore";
+
+import { getMasilDetail } from "@/lib/api/masil/client";
+import { GET_KEY } from "@/lib/api/queryKeys";
+import calculatePathCenter from "@/lib/utils/calculatePathCenter";
+
 import { GeoPosition } from "@/types/OriginDataType";
 import { TabType } from "./Log.types";
 
 interface useLogModelProps {
-  baseLocation: GeoPosition;
+  logId: string;
 }
 
-const useLogModel = ({ baseLocation }: useLogModelProps) => {
+const useLogModel = ({ logId }: useLogModelProps) => {
   const [tabIndex, setTabIndex] = useState(TabType.Memo);
   const [currentPinIndex, setCurrentPinIndex] = useState(0);
-  const [mapCenter, setMapCenter] = useState<GeoPosition>(baseLocation);
+  const [mapCenter, setMapCenter] = useState<GeoPosition>({ lat: 0, lng: 0 });
+
+  const { data: masilData } = useQuery({
+    queryKey: [GET_KEY.GET_LOG],
+    queryFn: () => getMasilDetail({ id: logId }),
+  });
+
+  const { sex, birthDate, height, weight, exerciseIntensity } = useMeStore();
+  const userInfo = { sex, birthDate, height, weight, exerciseIntensity };
+
+  const baseLocation = useMemo(() => {
+    if (masilData) {
+      return calculatePathCenter(masilData.path);
+    }
+
+    return { lat: 0, lng: 0 };
+  }, [masilData]);
+
+  useEffect(() => {
+    setMapCenter(baseLocation);
+  }, [baseLocation]);
 
   return {
+    masilData,
     tabIndex,
     setTabIndex,
     currentPinIndex,
     setCurrentPinIndex,
     mapCenter,
     setMapCenter,
+    baseLocation,
+    userInfo,
   };
 };
 
