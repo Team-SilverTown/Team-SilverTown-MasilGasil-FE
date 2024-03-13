81e879ea0245accc2c940caebb54605bb4bfca99
seunghyun <tmdgus7820@naver.com>
2024-03-14 00:03:15 +0900

    🏷️rename: PostMapSection 컴포넌트 components에서 sections 폴더로 이동

---
+++ src/app/post/[id]/components/index.ts
@@ -0,0 +1,4 @@
+export { default as PostKebabMenu } from "./PostKebabMenu/PostKebabMenu";
+export { default as PostMate } from "./PostMate/PostMate";
+export { default as PostMemo } from "./PostMemo/PostMemo";
+export { default as PostPin } from "./PostPin/PostPin";
