ae362d4ca71b0931ea1adfb57c10413155a46be8
Jaewoong Hwang <noa9925@gmail.com>
2024-03-13 12:27:10 +0900

    🐛fix: 아이폰에서 date가 잘못 출력되는 문제

--- src/app/diary/[id]/components/MasilDiarySheet/MasilDiarySheet.tsx
+++ src/app/diary/[id]/components/MasilDiarySheet/MasilDiarySheet.tsx
@@ -26,6 +26,7 @@ export interface MasilProps {
 const MasilDiarySheet = ({ isSheetOpen, setIsSheetOpen, masils, date }: MasilDiarySheetProps) => {
   const theme = useTheme();
   const dateArr = date?.toLocaleDateString("en-CA").split("-");
+  const [year, month, day] = [date?.getFullYear(), date?.getMonth(), date?.getDate()];
 
   return (
     <>
@@ -55,7 +56,7 @@ const MasilDiarySheet = ({ isSheetOpen, setIsSheetOpen, masils, date }: MasilDia
           <Sheet.Header />
           <Sheet.Content>
             <S.HeaderContainer>
-              <S.Text>{dateArr && `${dateArr[0]}년 ${dateArr[1]}월 ${dateArr[2]}일`}</S.Text>
+              <S.Text>{`${year}년 ${month}월 ${day}일`}</S.Text>
               <S.SubText
                 onClick={() => {
                   setIsSheetOpen(false);
