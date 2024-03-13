1797426a7f677adeb72c6c204a852b19b01df0d6
seunghyun <tmdgus7820@naver.com>
2024-03-14 00:10:41 +0900

    📝docs: MateList Dummy Data 수정 - API 데이터 나오기 전까지 사용할 임시 데이터 추가

--- src/app/post/[id]/Post.constants.ts
+++ src/app/post/[id]/Post.constants.ts
@@ -1,68 +1,40 @@
 import { MateDummyType } from "./Post.types";
 
-export const MATE_DUMMY_DATA: MateDummyType[] = [
-  {
-    id: 1,
-    title: "마실가실??",
-    content: "4명 정도 같이 산책할 친구 구합니다.",
-    gatheringPlace: {
-      point: {
-        lat: 10.2,
-        lng: 10.1,
-      },
-      detail: "약국 앞",
+export const MATE_DUMMY_DATA: MateDummyType = {
+  isEmpty: false,
+  contents: [
+    {
+      id: 1,
+      title: "같이 한강 가실분??",
+      gatheringAt: "2024-03-13T20:30:00.000000+09:00",
+      status: "OPEN",
+      capacity: 0, // 0 == 인원 상관 없음
+      authorId: 1,
+      authorNickname: "마실",
+      authorProfileUrl: null,
     },
-    gatherAt: "2024-03-04T10:05:33.000+09:00",
-    currentParticipants: {
-      participants: [
-        {
-          id: 1,
-          nickname: "산책초보",
-          profileUrl: null,
-        },
-        {
-          id: 2,
-          nickname: "산책개고수",
-          profileUrl: null,
-        },
-      ],
+    {
+      id: 2,
+      title: "여의도 한강 공원에서 라면 먹을 사람",
+      gatheringAt: "2024-03-14T20:30:00.000000+09:00",
+      status: "OPEN",
+      capacity: 4, // 0 == 인원 상관 없음
+      authorId: 1,
+      authorNickname: "마실",
+      authorProfileUrl: null,
     },
-    capacity: 3,
-    authorId: 3,
-    authorNickname: "닉네임",
-    authorProfileUrl: null,
-  },
-  {
-    id: 2,
-    title: "한강에서 자전거 타실분",
-    content: "한강에서 산책 후 라면에 맥주 때리실 분?",
-    gatheringPlace: {
-      point: {
-        lat: 10.2,
-        lng: 10.1,
-      },
-      detail: "약국 앞",
+    {
+      id: 3,
+      title: "오산천 가즈아!!",
+      gatheringAt: "2024-03-15T20:30:00.000000+09:00",
+      status: "CLOSED",
+      capacity: 0, // 0 == 인원 상관 없음
+      authorId: 1,
+      authorNickname: "마실",
+      authorProfileUrl: null,
     },
-    gatherAt: "2024-03-12T16:05:50.000+09:00",
-    currentParticipants: {
-      participants: [
-        {
-          id: 1,
-          nickname: "산책초보",
-          profileUrl: null,
-        },
-        {
-          id: 2,
-          nickname: "산책개고수",
-          profileUrl: null,
-        },
-      ],
-    },
-    capacity: 5,
-    authorId: 4,
-    authorNickname: "닉네임",
-    authorProfileUrl: null,
-  },
-];
+  ],
+  nextCursor: null, // 다음이 없으면 null, 있으면 마지막으로 조회된 id 문자열
+};
 
 export const TAB_CONTENTS = ["메모", "핀", "산책 메이트"];
--- src/app/post/[id]/Post.types.ts
+++ src/app/post/[id]/Post.types.ts
@@ -15,29 +15,18 @@ export interface UserDummyType {
 }
 
 export interface MateDummyType {
+  isEmpty: boolean;
+  contents: MateDummyContents[];
+  nextCursor: string | null;
+}
+
+export interface MateDummyContents {
   id: number;
   title: string;
-  content: string;
-  gatheringPlace: GatheringPlaceType;
-  gatherAt: string;
-  currentParticipants: CurrentParticipantsType;
+  gatheringAt: string;
+  status: "OPEN" | "CLOSED";
   capacity: number;
   authorId: number;
   authorNickname: string;
   authorProfileUrl: string | null;
-}
-
-interface GatheringPlaceType {
-  point: GeoPosition;
-  detail: string;
-}
-
-interface CurrentParticipantsType {
-  participants: participantsType[];
-}
-
-interface participantsType {
-  id: number;
-  nickname: string;
-  profileUrl: string | null;
 }
