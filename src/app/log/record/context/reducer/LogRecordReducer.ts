64c2b90ac4c6adc66b724459f43d24c2502055b1
Jaewoong Hwang <noa9925@gmail.com>
2024-03-13 19:25:34 +0900

    🚜change: GeoLocaion 상수값 수정

--- src/app/log/record/context/reducer/LogRecordReducer.ts
+++ src/app/log/record/context/reducer/LogRecordReducer.ts
@@ -38,7 +38,7 @@ type ActionsType =
 const MIN_INSERT_PIN_RANGE = 30; // M 단위
 
 const MIN_INSERT_PATH_RAGE = 25; // M 단위
-const MAX_INSERT_PATH_RAGE = 150; // M 단위
+const MAX_INSERT_PATH_RAGE = 50; // M 단위
 
 const logRecordReducer = (state: MasilRecordRequest, action: ActionsType) => {
   switch (action.type) {
