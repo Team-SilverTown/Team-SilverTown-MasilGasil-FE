1e1225d5c50cb27670d9fc53d4b024f8c7673c4b
seunghyun <tmdgus7820@naver.com>
2024-03-13 23:51:43 +0900

    🏷️rename: PostMateCard 컴포넌트를 MateCard 공용 컴포넌트로 수정

--- src/components/MateCard/MateCard.styles.ts
+++ src/components/MateCard/MateCard.styles.ts
@@ -1,11 +1,11 @@
 import { FONT_SIZE, FONT_WEIGHT } from "@/styles/theme";
 import styled from "styled-components";
 
-interface PostMateRecruitProps {
-  $isRecruit: boolean;
+interface MateRecruitProps {
+  $status: "OPEN" | "CLOSED";
 }
 
-export const PostMateCardLayout = styled.div`
+export const MateCardLayout = styled.div`
   position: relative;
   width: 100%;
   height: 9rem;
@@ -15,7 +15,7 @@ export const PostMateCardLayout = styled.div`
   box-shadow: rgba(0, 0, 0, 0.1) 1px 1px 5px;
 `;
 
-export const PostMateCardInfo = styled.div`
+export const MateCardInfo = styled.div`
   display: flex;
   align-items: center;
 
@@ -26,7 +26,7 @@ export const PostMateCardInfo = styled.div`
   }
 `;
 
-export const PostMateCardContent = styled.div`
+export const MateCardContent = styled.div`
   font-size: ${FONT_SIZE.MINI};
   font-weight: ${FONT_WEIGHT.BOLD};
   margin: 0.6rem 1rem 1rem 3.5rem;
@@ -34,20 +34,21 @@ export const PostMateCardContent = styled.div`
   text-overflow: ellipsis;
   white-space: nowrap;
 `;
-export const PostMateCardDate = styled.div`
+export const MateCardDate = styled.div`
   font-size: ${FONT_SIZE.MICRO};
   font-weight: ${FONT_WEIGHT.MEDIUM};
   text-align: right;
   color: ${(props) => props.theme.gray_300};
 `;
 
-export const PostMateRecruit = styled.mark<PostMateRecruitProps>`
+export const MateRecruit = styled.mark<MateRecruitProps>`
   position: absolute;
   top: 1rem;
   right: 1rem;
   width: 4rem;
   height: 2rem;
-  background-color: ${(props) => (props.$isRecruit ? props.theme.green_500 : props.theme.gray_500)};
+  background-color: ${(props) =>
+    props.$status === "OPEN" ? props.theme.green_500 : props.theme.gray_500};
   font-size: ${FONT_SIZE.MICRO};
   text-align: center;
   line-height: 2rem;
