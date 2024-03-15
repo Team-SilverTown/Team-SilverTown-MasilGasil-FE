3119dd781af77fa17b9e657c13861594cb32387f
joseunghyeon <tmdgus7820@naver.com>
2024-03-15 17:47:15 +0900

    ♻️refactor: useUserLocationStore UserAddress 값 추가

--- src/stores/useUserLocationStore.ts
+++ src/stores/useUserLocationStore.ts
@@ -1,4 +1,5 @@
 import { GeoPosition } from "@/types/OriginDataType";
+import { UserAddressType } from "@/types/OriginDataType/Location";
 import { create } from "zustand";
 
 // 강남역
@@ -9,12 +10,21 @@ const DEFAULT_USER_LOCATION: GeoPosition = {
 
 interface UseUserLocationStore {
   userLocation: GeoPosition;
+  userAddress: UserAddressType;
   setUserLocation: (param: { lat: number; lng: number }) => void;
+  setUserAddress: (location: UserAddressType) => void;
 }
 
 const useUserLocationStore = create<UseUserLocationStore>((set) => ({
   userLocation: DEFAULT_USER_LOCATION,
+  userAddress: {
+    depth1: "",
+    depth2: "",
+    depth3: "",
+    depth4: "",
+  },
   setUserLocation: ({ lat, lng }) => set(() => ({ userLocation: { lat, lng } })),
+  setUserAddress: (location) => set(() => ({ userAddress: location })),
 }));
 
 export default useUserLocationStore;
