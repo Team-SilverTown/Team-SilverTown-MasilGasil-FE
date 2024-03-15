3d84face857fa451df2bb0cbfe66c13f90f9b242
joseunghyeon <tmdgus7820@naver.com>
2024-03-15 17:51:38 +0900

    ♻️refactor: WalkStartButton 버튼 Style 수정 및 Components 폴더 정리 - index.ts로 관리 (모두 server component)

--- src/app/home/components/WalkStartButton/WalkStartButton.tsx
+++ src/app/home/components/WalkStartButton/WalkStartButton.tsx
@@ -11,13 +11,13 @@ const WalkStartButton = () => {
         variant="neumorp"
         buttonColor={Theme.lightTheme.green_500}
         textColor={Theme.lightTheme.white}
-        width={"calc(100% - 3rem)"}
+        width="100%"
         style={{
           position: "fixed",
           left: "50%",
           bottom: "7rem",
           transform: "translateX(-50%)",
-          maxWidth: `${CONTAINER.MAX_WIDTH}rem`,
+          maxWidth: `${CONTAINER.MAX_WIDTH - 3}rem`,
           height: "6rem",
           fontSize: `${FONT_SIZE.H6}`,
           fontWeight: `${FONT_WEIGHT.BOLD}`,
