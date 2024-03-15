d7dc834a691b7434d748f5a4bbe73b1427d190bf
Jaewoong Hwang <noa9925@gmail.com>
2024-03-15 16:23:31 +0900

    🚜change: 주석 및 사용하지 않는 더미데이터 제거

--- src/app/user/[id]/MyPage.controller.tsx
+++ src/app/user/[id]/MyPage.controller.tsx
@@ -1,4 +1,4 @@
-import { RECORDLIST_DUMMY_DATA, USER_PROFILE_EXCEPTION } from "./MyPage.constants";
+import { USER_PROFILE_EXCEPTION } from "./MyPage.constants";
 import MypageView from "./MyPage.view";
 import { getUserProfile } from "@/lib/api/User/server";
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
@@ -11,30 +11,12 @@ interface MyPageControllerProps {
   userId: number;
 }
 
-/**
- * @TODO
- * 1. Query Param으로부터 userId를 받아옴
- * 2. userId에 대한 get 요청을 보내 userInfo 데이터를 받아옴
- * 3. userId에 대한 관련 get 요청을 보내 PostData를 받아옴
- */
-
-/**
- * 내 통계
- * 내 산책
- * - 최근에 다녀온 산책
- * - 작성한 산책로
- * - 좋아요한 산책로
- */
-
 const MyPageController = async ({ userId }: MyPageControllerProps) => {
   const session = await getServerSession(authOptions);
 
   const userProfile = await getUserProfile(userId);
   const recentMasils = await getRecentMasils(session?.serviceToken!);
   const recentPosts = await getRecentPostsById(session?.serviceToken!, userId);
-
-  console.log(recentMasils);
-  console.log(recentPosts);
 
   const boardLists: MyRecordListType[] = [
     {
