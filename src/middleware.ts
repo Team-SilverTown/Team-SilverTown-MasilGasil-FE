20a7543be30914b23f4ed83c8ec327ccc2e05f05
Zoody <reopard506@gmail.com>
2024-03-16 11:13:04 +0900

    🐛fix: middleware bypass 로직 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,18 +7,21 @@ export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|fonts|images).*)"],
 };
 
+const bypassPaths = ["/manifest*", "/swe-worker*", "/sw.js", "/workbox-*", "/icons*", "/fonts*"];
 const protectedPaths = ["/setting*"]; // 로그인이 필요한 페이지 목록
 const publicPaths = ["/signup*", "/auth*"]; // 로그인이 되면 접근할 수 없는 페이지 목록
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request });
   const currentPath = request.nextUrl.pathname;
 
-  // console.log("middleware", token);
+  const isBypassPaths = pathAbleCheck(bypassPaths, currentPath);
+  if (isBypassPaths) return;
 
-  const protectedAcceesInable = pathAbleCheck(protectedPaths, currentPath);
+  const token = await getToken({ req: request });
 
-  if (!token || (!token.nickname && protectedAcceesInable)) {
+  const protectedPathsAccessInable = pathAbleCheck(protectedPaths, currentPath);
+
+  if (!token || (!token.nickname && protectedPathsAccessInable)) {
     const url = request.nextUrl.clone();
     url.pathname = "/";
 
@@ -29,7 +32,6 @@ export async function middleware(request: NextRequest) {
 
   // 인증된 유저인 경우 publicPaths 에 접근할 수 없도록 함.
   if (token && token.nickname && publicPathsAccessInable) {
-    // console.log("middleware block");
     const url = request.nextUrl.clone();
     url.pathname = "/";
     return NextResponse.redirect(url);
