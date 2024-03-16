8ef73de6db33acb024c59ac293ae8318c4e905bf
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-03-16 19:08:07 +0900

    🎨style: 높이 개선

--- src/components/navigators/TopNavigator/TopNavigator.styles.ts
+++ src/components/navigators/TopNavigator/TopNavigator.styles.ts
@@ -15,6 +15,8 @@ export const TopNavigatorContainer = styled.nav<any>`
   align-items: center;
 
   background-color: ${(props) => props.theme.background_color};
+
+  border: 1px solid red;
 `;
 
 export const TopNavLeftSection = styled.section`
