44195d294ff48c5ff1cda88970d4d834154d6cfa
Zoody <reopard506@gmail.com>
2024-03-18 00:54:34 +0900

    ♻️refactor: react-virtualized 도입

--- src/app/explore/sections/ListSection/ListSection.tsx
+++ src/app/explore/sections/ListSection/ListSection.tsx
@@ -1,10 +1,12 @@
-import React, { memo, useEffect } from "react";
+import React, { CSSProperties, memo, useCallback, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
+import { List as VList, AutoSizer, InfiniteLoader } from "react-virtualized";
 
 import { ListCard } from "@/components";
 import { PostListItem } from "@/types/OriginDataType/Post";
 import parseLocationObject from "@/utils/parseLocation";
 import { useRouter } from "next/navigation";
+import LoadingDots from "@/components/LoadingDots";
 
 interface ListSectionProps {
   id: "post" | "mate";
@@ -23,19 +25,6 @@ const ListSection = memo(function List({
 }: ListSectionProps) {
   const router = useRouter();
 
-  const [ref, inView] = useInView({
-    rootMargin: "20%",
-  });
-
-  // console.log(hasNextPage);
-
-  useEffect(() => {
-    if (inView && !isFetchingNextPage && hasNextPage) {
-      fetchNextPage && fetchNextPage();
-      console.log("inview");
-    }
-  }, [inView]);
-
   const cardClickHandler = (id: number) => {
     router.push(`/post/${id}`);
   };
@@ -53,45 +42,95 @@ const ListSection = memo(function List({
     );
   }
 
-  return (
-    <div
-      id={id}
-      className="w-full h-full"
-    >
-      {data && data.length > 0 && (
-        <>
-          <ul className="p-4 space-y-8">
-            {data.map((item) => (
-              <li key={item.id}>
-                <ListCard
-                  isRecruit={item.hasMate}
-                  isLiked={item.liked}
-                  likeCount={item.likeCount}
-                  title={item.title}
-                  content={item.content}
-                  totalTime={item.totalTime}
-                  distance={item.distance}
-                  thumbnailUrl={item.thumbnailUrl}
-                  address={parseLocationObject(item.address) ?? ""}
-                  onCardClickHandler={() => cardClickHandler(item.id)}
-                />
-              </li>
-            ))}
-          </ul>
-          <div
-            ref={ref}
-            className="w-full p-4"
-          />
-        </>
-      )}
+  if (data && fetchNextPage) {
+    // If there are more items to be loaded then add an extra row to hold a loading indicator.
+    const rowCount = hasNextPage ? data.length + 1 : data.length;
+    // Only load 1 page of items at a time.
+    // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
+    const loadMoreRows = isFetchingNextPage ? () => {} : fetchNextPage;
+    // Every row is loaded except for our loading indicator row.
+    const isRowLoaded = ({ index }: { index: number }) => !hasNextPage || index < data.length;
 
-      {(!data || data.length <= 0) && (
-        <div className="flex justify-center">
-          <span className="font-medium text-medium">해당 지역에 대한 검색 결과가 없어요.</span>
-        </div>
-      )}
-    </div>
-  );
+    const rowRenderer = useCallback(
+      ({ key, index, style }: any) => {
+        const item = data[index];
+
+        let content;
+
+        if (!isRowLoaded({ index })) {
+          content = <LoadingDots />;
+        } else {
+          content = (
+            <ListCard
+              isRecruit={item.hasMate}
+              isLiked={item.liked}
+              likeCount={item.likeCount}
+              title={item.title}
+              content={item.content}
+              totalTime={item.totalTime}
+              distance={item.distance}
+              thumbnailUrl={item.thumbnailUrl}
+              address={parseLocationObject(item.address) ?? ""}
+              onCardClickHandler={() => cardClickHandler(item.id)}
+            />
+          );
+        }
+
+        return (
+          <div
+            key={key}
+            style={style}
+            className="p-4"
+          >
+            {content}
+          </div>
+        );
+      },
+      [data, cardClickHandler],
+    );
+
+    return (
+      <div
+        id={id}
+        className="w-full h-full"
+      >
+        {data && data.length > 0 && (
+          <>
+            <AutoSizer>
+              {({ height, width }) => (
+                <InfiniteLoader
+                  isRowLoaded={isRowLoaded}
+                  // @ts-ignore
+                  loadMoreRows={loadMoreRows}
+                  rowCount={rowCount}
+                >
+                  {({ onRowsRendered, registerChild }) => (
+                    <VList
+                      ref={registerChild}
+                      onRowsRendered={onRowsRendered}
+                      width={width}
+                      height={height}
+                      rowCount={data.length}
+                      rowHeight={250 + 20} // card height + padding vertical 20
+                      rowRenderer={rowRenderer}
+                      className="scrollbar-hide"
+                      style={{ paddingBottom: "2rem" }}
+                    />
+                  )}
+                </InfiniteLoader>
+              )}
+            </AutoSizer>
+          </>
+        )}
+
+        {(!data || data.length <= 0) && (
+          <div className="flex justify-center">
+            <span className="font-medium text-medium">해당 지역에 대한 검색 결과가 없어요.</span>
+          </div>
+        )}
+      </div>
+    );
+  }
 });
 
 export default ListSection;
