e846fb8bbfc7873b78d8b3fb2b24f65ed2d649c2
오원주 <reopard506@gmail.com>
2024-03-17 23:46:11 +0900

    [Fix] auth service token 에러 픽스 (#249)
    * 🐛fix: auth service token 에러 픽스
    
    * 🚜change:  불필요 주석 제거

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,45 +7,43 @@ export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|fonts|images).*)"],
 };
 
-const bypassPaths = ["/manifest*", "/swe-worker*", "/sw.js", "/workbox-*", "/icons*", "/fonts*"];
+const bypassPaths = [
+  "/",
+  "/manifest*",
+  "/swe-worker*",
+  "/sw.js",
+  "/workbox-*",
+  "/icons*",
+  "/fonts*",
+  "/favicon*",
+];
 const protectedPaths = ["/setting*"]; // 로그인이 필요한 페이지 목록
 const publicPaths = ["/signup*", "/auth*"]; // 로그인이 되면 접근할 수 없는 페이지 목록
 
-const test = ["/home*", "/user*"];
-
-const NEXT_AUTH_URL = process.env.NEXTAUTH_URL;
-
 export async function middleware(request: NextRequest) {
-  const referer = request.headers.get("x-forwarded-host");
-
   const currentPath = request.nextUrl.pathname;
 
+  // 미들웨어 인터셉트가 필요없는 경우 bypass
   const isBypassPaths = pathAbleCheck(bypassPaths, currentPath);
   if (isBypassPaths) return;
 
   const token = await getToken({ req: request });
 
+  // 미인증, 가인증 유저인 경우, 보호되는 경로로 접근할 수 없도록 함.
   const protectedPathsAccessInable = pathAbleCheck(protectedPaths, currentPath);
-
   if (!token || (!token.nickname && protectedPathsAccessInable)) {
     const url = request.nextUrl.clone();
     url.pathname = "/";
 
-    if (pathAbleCheck(test, currentPath)) {
-      return NextResponse.redirect(url);
-    }
-
-    if (referer && NEXT_AUTH_URL?.endsWith(referer)) return;
     return NextResponse.redirect(url);
   }
 
+  // 인증된 유저인 경우, 인증 유저는 접근할 수 없는 경로에 대한 블로킹 (유저인증 관련 등F)
   const publicPathsAccessInable = pathAbleCheck(publicPaths, currentPath);
-
   if (token && token.nickname && publicPathsAccessInable) {
     const url = request.nextUrl.clone();
     url.pathname = "/";
 
-    if (referer && NEXT_AUTH_URL?.endsWith(referer)) return;
     return NextResponse.redirect(url);
   }
 
