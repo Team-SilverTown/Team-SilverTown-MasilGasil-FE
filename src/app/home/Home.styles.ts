4f5cce2ce3f6a4dc8b5fcb5b8912c1ccf3c741f3
seunghyun <tmdgus7820@naver.com>
2024-03-18 20:28:21 +0900

    🎨style: HomePage 상단 패딩(여백) 수정 - 조금 더 여유 있게 수정했습니다.

--- src/app/home/Home.styles.ts
+++ src/app/home/Home.styles.ts
@@ -1,4 +1,4 @@
-export const HomePageContainer = `w-full pt-28 pb-80 box-border select-none`;
+export const HomePageContainer = `w-full pt-32 pb-80 box-border select-none`;
 
 export const MyInfoSection = `
   px-6
