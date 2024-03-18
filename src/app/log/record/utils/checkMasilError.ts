a7df574bc97902562f734d45f56dae6f2786ae1f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-03-18 13:14:59 +0900

    ♻️refactor: LogRecord 에러메세지 상수화

--- src/app/log/record/utils/checkMasilError.ts
+++ src/app/log/record/utils/checkMasilError.ts
@@ -1,12 +1,14 @@
+import { LOG_RECORD_ERROR_MESSAGE } from "../LogRecord.constants";
+
 const checkMasilErrorCode = (errorCode: string) => {
   console.log(errorCode);
 
   switch (errorCode) {
     case "20016002":
-      return "산책하신 경로가 너무 적습니다.<br>100m 이상부터 산책을 기록하실 수 있습니다.";
+      return LOG_RECORD_ERROR_MESSAGE.MIN_LENGTH;
 
     default:
-      return `산책로 저장에 오류가 발생하였습니다.<br>잠시 후 다시 시도해주세요.`;
+      return LOG_RECORD_ERROR_MESSAGE.DEFAULT;
   }
 };
 
