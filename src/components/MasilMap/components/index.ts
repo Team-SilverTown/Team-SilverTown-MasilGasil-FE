784df2b2b3cac6981f72131dd271c91d15113ca0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-03-20 02:20:50 +0900

    ✨feat: mate 모집장소 Pin / post정보 연결

--- src/components/MasilMap/components/index.ts
+++ src/components/MasilMap/components/index.ts
@@ -2,3 +2,4 @@ export { default as CenterMarker } from "./CenterMarker/CenterMarker";
 export { default as CustomPin } from "./CustomPin/CustomPin";
 export { default as NavigationPin } from "./NavigationPin/NavigationPin";
 export { default as PathLine } from "./PathLine/PathLine";
+export { default as MatePointPin } from "./MatePointPin/MatePointPin";
--- src/lib/api/Mate/server.ts
+++ src/lib/api/Mate/server.ts
@@ -5,6 +5,7 @@ import { MATE } from "../endPoints";
 export async function getMateDetail(id: string) {
   const response = await GET<MateDetailResponse>({
     endPoint: `${MATE.GET_DETAIL}/${id}`,
+    options: { cache: "no-store" },
   });
 
   return response;
