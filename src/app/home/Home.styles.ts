e1d3598ad47b0e475e3295818899d750a3446129
seunghyun <tmdgus7820@naver.com>
2024-03-19 12:40:12 +0900

    🎨style: MyWalkRecord 컴포넌트 style 수정

--- src/app/home/Home.styles.ts
+++ src/app/home/Home.styles.ts
@@ -1,3 +1,3 @@
-export const HomePageContainer = `w-full pt-28 pb-80 box-border select-none`;
+export const HomePageContainer = `w-full pt-24 pb-80 box-border select-none`;
 
 export const HomeLogo = `font-maplestory ml-6 font-bold text-green-500`;
