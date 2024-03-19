89ce9f0744db4ac751d2bc86910e44eb149ffcb6
seunghyun <tmdgus7820@naver.com>
2024-03-20 02:01:01 +0900

    ✨feat: 현재 산책 리스트에 대한 mate 모집 데이터 추가

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import { getPostDetail } from "@/lib/api/Post/server";
 import PostController from "./Post.controller";
+import { getMateDetail } from "@/lib/api/Mate/server";
 
 interface PostDetailProps {
   params: {
@@ -14,8 +15,9 @@ const PostDetail = async ({ params }: PostDetailProps) => {
 
   const session = await getServerSession(authOptions);
   const postData = await getPostDetail(session?.serviceToken!, id);
+  const mateData = await getMateDetail(id);
 
-  if (!postData) {
+  if (!postData || !mateData) {
     return;
   }
 
@@ -23,6 +25,7 @@ const PostDetail = async ({ params }: PostDetailProps) => {
     <PostController
       postId={id}
       postData={postData}
+      mateData={mateData}
     />
   );
 };
