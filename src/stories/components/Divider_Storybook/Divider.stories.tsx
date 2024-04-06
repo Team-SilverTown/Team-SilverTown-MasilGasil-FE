df0bb24c5d1bc85dc156af3dba1adbb188e275ac
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-07 02:08:56 +0900

    ✨feat: Divider Story 추가

---
+++ src/stories/components/Divider_Storybook/Divider.stories.tsx
@@ -0,0 +1,52 @@
+import { Divider } from "@/components/Divider";
+import { Meta, StoryObj } from "@storybook/react";
+
+/**
+ * **공통 Divider 컴포넌트**
+ *
+ * ### Props
+ * - **isColumn : **boolean 값을 통해 구분선의 세로 여부를 정의합니다.
+ */
+const meta = {
+  title: "Components/Divider",
+  component: Divider,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+
+  args: {
+    isColumn: false,
+  },
+
+  argTypes: {
+    style: {
+      control: "null",
+    },
+  },
+
+  decorators: [
+    (Story) => (
+      <section className="w-[60rem] h-[10rem] flex items-center justify-center">
+        <Story />
+      </section>
+    ),
+  ],
+} satisfies Meta<typeof Divider>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const IsColumn_False: Story = {
+  args: {
+    isColumn: false,
+  },
+};
+
+export const IsColumn_True: Story = {
+  args: {
+    isColumn: true,
+  },
+};
