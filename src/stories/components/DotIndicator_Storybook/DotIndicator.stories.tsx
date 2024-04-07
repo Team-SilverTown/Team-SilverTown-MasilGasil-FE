68a44c0e991c5c31b85f43377acbde99e0f3616c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-07 15:17:43 +0900

    ✨feat: DotIndicator Story 생성

---
+++ src/stories/components/DotIndicator_Storybook/DotIndicator.stories.tsx
@@ -0,0 +1,35 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import DotIndicator_Storybook from "./DotIndicator_Storybook";
+
+/**
+ * **공통 DotIndicator 컴포넌트**
+ *
+ * ### Props
+ * - **current : ** DotIndicator의 현재 선택되어진 Index를 전달받습니다.
+ * - **length : **DotIndicator의 생성할 Dot 갯수를 전달받습니다.
+
+ */
+const meta = {
+  title: "Components/DotIndicator",
+  component: DotIndicator_Storybook,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+
+  args: {
+    current: 3,
+    length: 7,
+  },
+
+  argTypes: {
+    current: { control: "null" },
+    length: { control: "null" },
+  },
+} satisfies Meta<typeof DotIndicator_Storybook>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
