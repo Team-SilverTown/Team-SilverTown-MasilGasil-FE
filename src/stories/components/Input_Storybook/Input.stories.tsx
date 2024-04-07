b8bcf7fc5348c740f4160126058c8e805835fa52
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-07 20:43:57 +0900

    ♻️refactor: Input Storybook Description 개선

--- src/stories/components/Input_Storybook/Input.stories.tsx
+++ src/stories/components/Input_Storybook/Input.stories.tsx
@@ -21,10 +21,32 @@ const meta = {
   tags: ["autodocs"],
 
   argTypes: {
-    register: { control: "null", description: "React-Hook-Form의 register 전달" },
-    width: { control: "number" },
-    kind: { control: "null" },
-    style: { control: "null" },
+    register: {
+      control: "null",
+      description: "React-Hook-Form의 register 전달",
+    },
+
+    isInvalid: {
+      description: "boolean값을 통해 Input의 Invalid상태를 전달 받습니다.",
+    },
+
+    required: {
+      description: "boolean값을 통해 현재 Input의 필수 여부를 전달받습니다.",
+    },
+
+    width: {
+      control: "number",
+      description: "현재 Input의 넓이를 전달합니다.",
+    },
+
+    kind: {
+      control: "null",
+    },
+
+    style: {
+      control: "null",
+      description: "기본 CSS 스타일",
+    },
   },
 } satisfies Meta<typeof Input>;
 
