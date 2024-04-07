ae8203d7ffddaa37933883d97b05586a6d809cc3
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-07 20:48:14 +0900

    ♻️refactor: DropDownMenu Storybook Description 수정

--- src/stories/components/DropDownMenu_Storybook/DropDownMenu.stories.tsx
+++ src/stories/components/DropDownMenu_Storybook/DropDownMenu.stories.tsx
@@ -3,19 +3,12 @@ import { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 
 /**
- * **공통 Button 컴포넌트**
+ * **공통 DropDownMenu 컴포넌트**
  *
  * ### Props
- * - **variant : **v버튼의 생김새를 지정합니다.
- * - **buttonColor : **버튼의 색상을 정의합니다.
- * - **textColor : **버튼 내부 텍스트 색상을 정의합니다.
- * - **width : **버튼의 너비를 정의합니다.
- * - **disabled : **boolean 값을 전달하여 버튼의 바활성화 여부를 정의합니다.
- * - **isLoading : **로딩 상테( boolean )값을 전달하여 로딩에 따른 버튼 디자인을 정의합니다.
- * - **useRipple : **boolean 값에따라 버튼 클릭시 Ripple 디자인을 사용할지 정의합니다.
- * - **rippleColor : **ripple 동작시 색상을 정의합니다.
- * - **onClickHandler : **버튼 클릭시 수행될 메서드를 전달받습니다.
- * - **handlerDelay : **버튼 클릭시 전달받은 onClickHandler 메서드의 실행을 지연시킵니다. - ms
+ * - **isEdit : **boolean값을 통해 메뉴 리스트중 수정 버튼의 존재 여부를 전달받습니다.
+ * - **onEdit : **수정 버튼 클릭시 수행될 메서드를 전달받습니다.
+ * - **onDelete : **삭제 버튼 클릭시 수행될 메서드를 전달받습니다.
  */
 const meta = {
   title: "Components/DropDownMenu",
@@ -29,6 +22,20 @@ const meta = {
     isEdit: true,
     onEdit: fn(),
     onDelete: fn(),
+  },
+
+  argTypes: {
+    isEdit: {
+      description: "boolean값을 통해 메뉴 리스트중 수정 버튼의 존재 여부를 전달받습니다.",
+    },
+
+    onEdit: {
+      description: "수정 버튼 클릭시 수행될 메서드를 전달받습니다.",
+    },
+
+    onDelete: {
+      description: "삭제 버튼 클릭시 수행될 메서드를 전달받습니다.",
+    },
   },
 
   decorators: [
