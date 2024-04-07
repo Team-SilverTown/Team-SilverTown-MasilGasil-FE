02187842264d89606d71af3cf3e373a6074c1cfc
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-07 20:16:28 +0900

    ✨feat: Input Story 추가

---
+++ src/stories/components/Input_Storybook/Input.stories.tsx
@@ -0,0 +1,39 @@
+import { Input } from "@/components";
+import { Meta } from "@storybook/react";
+
+/**
+ * **공통 Input 컴포넌트**
+ *
+ * ### Props
+ * - **register : **React-Hook-Form의 register를 전달받습니다.
+ * - **isInvalid : **boolean값을 통해 Input의 Invalid상태를 전달 받습니다.
+ * - **required : **boolean값을 통해 현재 Input의 필수 여부를 전달받습니다.
+ * - **width : **현재 Input의 넓이를 전달합니다.
+ */
+const meta = {
+  title: "Components/Input",
+  component: Input,
+
+  parameters: {
+    layout: "centered",
+  },
+
+  tags: ["autodocs"],
+
+  argTypes: {
+    register: { control: "null", description: "React-Hook-Form의 register 전달" },
+    width: { control: "number" },
+    kind: { control: "null" },
+    style: { control: "null" },
+  },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+
+export const Default = {};
+
+export const IsInvalid_True = {
+  args: {
+    isInvalid: true,
+  },
+};
