e7fe4f1cd2ac29f3af9afa8591d958d513014cb5
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-16 22:19:22 +0900

    🔥Hotfix: getME 변경으로 인한 배포 실패 수정

--- src/app/user/[id]/setting/edit/page.tsx
+++ src/app/user/[id]/setting/edit/page.tsx
@@ -1,20 +1,11 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import { TopNavigator } from "@/components/navigators/TopNavigator";
 import { GoBackButton } from "@/components/navigators/TopNavigator/components";
 import { getMe } from "@/lib/api/User/server";
 
 import UserEditView from "./UserEdit.view";
 
-import { getServerSession } from "next-auth";
-
 const UserEdit = async () => {
-  const session = await getServerSession(authOptions);
-
-  if (!session?.serviceToken) {
-    return;
-  }
-
-  const defaultData = await getMe(session.serviceToken);
+  const defaultData = await getMe();
 
   if (!defaultData) {
     return;
