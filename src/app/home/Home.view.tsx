5736c406db4334fd903c617d242c68ddcb48d5da
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-23 18:33:48 +0900

    ♻️refactor: HomeView convention

--- src/app/home/Home.view.tsx
+++ src/app/home/Home.view.tsx
@@ -1,5 +1,3 @@
-import * as S from "./Home.styles";
-
 import { TopNavigator } from "@/components/navigators/TopNavigator";
 import { ProfileResponse } from "@/types/Response";
 import { PostMoreListResponse } from "@/types/Response/Post";
@@ -23,14 +21,14 @@ const HomeView = ({
       <TopNavigator
         leftChildren={
           <h1
-            className={S.HomeLogo}
+            className="ml-6 font-maplestory font-bold text-green-500"
             style={{ fontSize: "2.2rem" }}
           >
             마실가실
           </h1>
         }
       />
-      <div className={S.HomePageContainer}>
+      <div className="box-border w-full select-none pb-80 pt-24">
         <MyInfo userInfo={userInfo} />
         <WalkList
           MyLikeWalkingTrailsList={MyLikeWalkingTrailsList}
