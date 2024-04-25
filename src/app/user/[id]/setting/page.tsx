39affd15f0d27ad83fb6932c483a1dd131487ace
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-26 03:38:23 +0900

    [ RE : Refactoring ] User Setting 페이지 낙관적 업데이트 및 Debounce 적용 (#324)

--- src/app/user/[id]/setting/page.tsx
+++ src/app/user/[id]/setting/page.tsx
@@ -1,16 +1,32 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import { TopNavigator } from "@/components/navigators/TopNavigator";
 import { GoBackButton } from "@/components/navigators/TopNavigator/components";
+import { getMe } from "@/lib/api/User/server";
 
 import UserSettingView from "./UserSetting.view";
 
-const Setting = () => {
+import { getServerSession } from "next-auth";
+
+const Setting = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.serviceToken) {
+    return;
+  }
+
+  const meData = await getMe(session.serviceToken);
+
+  if (!meData) {
+    return;
+  }
+
   return (
     <>
       <TopNavigator
         leftChildren={<GoBackButton />}
         title={"설정"}
       />
-      <UserSettingView />
+      <UserSettingView meData={meData} />
     </>
   );
 };
