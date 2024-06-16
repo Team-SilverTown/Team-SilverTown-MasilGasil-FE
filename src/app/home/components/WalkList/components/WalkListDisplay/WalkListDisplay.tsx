05cd0bdb8c1a0074a32fd4ac26b71465f0381576
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-17 00:42:53 +0900

    [ Feature ] 로그인 모달 구현 (#332)
    * ✨feat: AccessLoginModal 생성
    
    * ✨feat: AccessLoginModal 구현
    
    * 🐛fix: Home 관련 client 문제 임시 해결

--- src/app/home/components/WalkList/components/WalkListDisplay/WalkListDisplay.tsx
+++ src/app/home/components/WalkList/components/WalkListDisplay/WalkListDisplay.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+// TODO - 추후 모든 Tailwind 마이그레이션 작업후 use client 제거
 import { FONT_SIZE, FONT_WEIGHT } from "@/styles/theme";
 
 import { LogDetailCard } from "@/components";
---
+++ src/components/modalViews/AccessLoginModal/AccessLoginModal.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import Lottie from "react-lottie";
+
+import tailwindConfig from "@/../tailwind.config";
+import Button from "@/components/Button";
+import ModalLayout from "@/components/Modal/ModalLayout";
+import { Message } from "@/components/icons";
+import { useUI } from "@/components/uiContext/UiContext";
+
+import ACCESS_LOGIN_ANIMATION from "./AccessLoginAnimationData.json";
+
+import { signIn } from "next-auth/react";
+import resolveConfig from "tailwindcss/resolveConfig";
+
+const AccessLoginModal = () => {
+  const { closeModal, showLoadingSpinner } = useUI();
+
+  const { theme } = resolveConfig(tailwindConfig);
+
+  return (
+    <ModalLayout>
+      <section className="flex w-[30rem] select-none flex-col gap-[2rem]  pt-[2rem]">
+        <Lottie
+          options={{
+            loop: false,
+            autoplay: true,
+            animationData: ACCESS_LOGIN_ANIMATION,
+            rendererSettings: {
+              preserveAspectRatio: "xMidYMid slice",
+            },
+          }}
+          height={180}
+          width={200}
+          isClickToPauseDisabled={true}
+        />
+
+        <p className="w-full  whitespace-pre-line text-center text-h6 font-bold leading-[2.6rem]">
+          {"현재 서비스는 로그인하신 후 \n 이용하실 수 있습니다."}
+        </p>
+
+        <Button
+          buttonColor={"#FEE500"}
+          width="100%"
+          style={{ margin: "auto", borderRadius: 12, padding: "1.5rem 1.5rem" }}
+          useRipple
+          onClickHandler={() => {
+            showLoadingSpinner();
+            signIn("kakao", { redirect: true, callbackUrl: "/auth/kakao" });
+          }}
+        >
+          <Message className="mr-2 h-7 w-7" />
+          <span className="text-large font-medium ">카카오 로그인</span>
+        </Button>
+
+        <Button
+          buttonColor={theme.colors.gray_300}
+          textColor={theme.colors.text_secondary_color}
+          width="100%"
+          style={{ margin: "auto", borderRadius: 12, padding: "1.5rem 1.5rem" }}
+          useRipple
+          onClickHandler={() => {
+            closeModal();
+          }}
+        >
+          <span className="text-large font-medium ">닫기</span>
+        </Button>
+      </section>
+    </ModalLayout>
+  );
+};
+
+export default AccessLoginModal;
