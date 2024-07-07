132ff850c11e8ae5e6309e5959d653740ee124f8
Jaewoong Hwang <noa9925@gmail.com>
2024-07-07 19:20:22 +0900

    🚜change: 아이콘 asset 추가

--- src/app/home/Home.view.tsx
+++ src/app/home/Home.view.tsx
@@ -1,3 +1,4 @@
+import LogoM from "@/components/icons/LogoM";
 import { TopNavigator } from "@/components/navigators/TopNavigator";
 import { ProfileResponse } from "@/types/Response";
 import { PostMoreListResponse } from "@/types/Response/Post";
@@ -17,16 +18,7 @@ const HomeView = ({
 }: HomeViewProps) => {
   return (
     <>
-      <TopNavigator
-        leftChildren={
-          <h1
-            className="ml-6 font-maplestory font-bold text-green-500"
-            style={{ fontSize: "2.2rem" }}
-          >
-            마실가실
-          </h1>
-        }
-      />
+      <TopNavigator leftChildren={<LogoM />} />
       <div className="box-border w-full select-none pb-80 pt-24">
         <MyInfo userInfo={userInfo} />
         <WalkList
