db668e6e84d1a833c9cdac42e9e0f77a87d83d3a
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-20 21:34:32 +0900

    ♻️refactor: Styled components 제거

--- src/components/navigators/BottomNavigator/BottomNavigator.tsx
+++ src/components/navigators/BottomNavigator/BottomNavigator.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import * as S from "./BottomNavigator.styles";
-
-import { useEffect } from "react";
+import { CONTAINER, NAV_HEIGHT } from "@/styles/theme";
 
 import userProfile from "@/assets/userProfile.svg";
 import { Avatar } from "@/components";
@@ -14,6 +12,7 @@ import { BOTTOM_NAV_ITEMS } from "./BottomNavigator.constants";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { twJoin } from "tailwind-merge";
 
 const BottomNavigator = () => {
   const currentPathName = usePathname();
@@ -29,11 +28,18 @@ const BottomNavigator = () => {
   const isPathActive = (path: string) => currentPathName.includes(path);
 
   return (
-    <S.BottomNavContainer>
+    <nav
+      className="max-w fixed bottom-0 z-navigator flex w-full justify-around border-t-[0.1rem] border-transparent_10 bg-background_color"
+      style={{
+        maxWidth: `${CONTAINER.MAX_WIDTH}rem`,
+        height: `${NAV_HEIGHT + 2}rem`,
+        padding: `${CONTAINER.PADDING_HORIZONTAL}rem`,
+      }}
+    >
       {BOTTOM_NAV_ITEMS.map(({ path, icon, activeIcon, label, isProfile, isIdRequired }) => (
         <div
           key={path}
-          className="flex flex-1 justify-center items-center"
+          className="flex flex-1 items-center justify-center"
         >
           <Link
             href={`${path}${isIdRequired && userId ? `/${userId}` : ""}`}
@@ -41,9 +47,22 @@ const BottomNavigator = () => {
             className="px-5"
             scroll={false}
           >
-            <S.BottomNavItem $active={isPathActive(path)}>
+            <div
+              className={twJoin(
+                "group flex cursor-pointer flex-col items-center justify-center text-center text-mini",
+                isPathActive(path) ? "text-green_500" : "text-gray_300 hover:text-green_500",
+              )}
+              style={{
+                paddingBottom: "env(safe-area-inset-bottom)",
+              }}
+            >
               {isProfile && (
-                <S.ProfileCircle $active={isPathActive(path)}>
+                <div
+                  className={twJoin(
+                    "mb-[0.4rem] flex h-[2.4rem] w-[2.4rem] cursor-pointer items-center justify-center rounded-[50%]",
+                    isPathActive(path) ? "bg-green_500" : "bg-gray_300 group-hover:bg-green_500",
+                  )}
+                >
                   <Avatar
                     size="ms"
                     src={profileImg ? profileImg : userProfile}
@@ -51,15 +70,15 @@ const BottomNavigator = () => {
                       pointerEvents: "none",
                     }}
                   />
-                </S.ProfileCircle>
+                </div>
               )}
               {!isProfile && isPathActive(path) ? activeIcon : icon}
               {label}
-            </S.BottomNavItem>
+            </div>
           </Link>
         </div>
       ))}
-    </S.BottomNavContainer>
+    </nav>
   );
 };
 
