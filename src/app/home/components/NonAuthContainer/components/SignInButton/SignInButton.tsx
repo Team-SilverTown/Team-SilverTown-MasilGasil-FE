8ac22194e56016c89ea3596e950e866e33df0efc
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-25 21:43:18 +0900

    🐛fix: 로그인 페이지 경로 수정

--- src/app/home/components/NonAuthContainer/components/SignInButton/SignInButton.tsx
+++ src/app/home/components/NonAuthContainer/components/SignInButton/SignInButton.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const SignInButton = () => {
   return (
-    <Link href="/login">
+    <Link href="/">
       <section className="flex flex-shrink-0 flex-grow-0 cursor-pointer select-none justify-center gap-2 rounded-[3rem] bg-white bg-opacity-30 px-9 py-4 shadow-[inset_0px_0px_0px_2px_rgba(255,255,255,0.3)]">
         <span className="flex  gap-5 text-large font-bold text-white">마실가실 로그인하기</span>
       </section>
