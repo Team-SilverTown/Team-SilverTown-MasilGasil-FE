f25aa64be7df0763e255de9c73b1c7e510550cfb
seunghyun <tmdgus7820@naver.com>
2024-09-20 18:01:22 +0900

    ♻️refactor: Post 페이지 좋아요 기능 분리 및 성능 개선 - debounce 함수 적용으로 최종 상태에 따른 API 호출 (마지막 한 번만 호출함) - 만약 현재 좋아요 상태와 동일한 경우 API 호출되지 않도록 조건문 추가 - 낙관적 업데이트 되도록 수정

---
+++ src/app/post/[id]/components/PostMemo/hooks/index.ts
@@ -0,0 +1,1 @@
+export { default as useLikeMutation } from "./useLikeMutation/useLikeMutation";
---
+++ src/app/post/[id]/components/PostMemo/hooks/useLikeMutation/useLikeMutation.ts
@@ -0,0 +1,80 @@
+import { useCallback, useState } from "react";
+
+import { useUI } from "@/components/uiContext/UiContext";
+import { fetchPostLikedToggle } from "@/lib/api/Post/client";
+import { POST_KEY } from "@/lib/api/queryKeys";
+import useAuthStore from "@/lib/stores/useAuthStore";
+import checkErrorCode from "@/lib/utils/checkErrorCode";
+import { LikesResponse } from "@/types/Response";
+import { useMutation } from "@tanstack/react-query";
+
+import { debounce } from "lodash";
+import { useRouter } from "next/navigation";
+
+interface UseLikeMutationProps {
+  id: number;
+  isLiked: boolean;
+  likeCount: number;
+}
+
+const useLikeMutation = ({ id, isLiked, likeCount }: UseLikeMutationProps) => {
+  const router = useRouter();
+  const [isLike, setIsLike] = useState(isLiked);
+  const [likes, setLikes] = useState(likeCount);
+  const [isCurrentLiked, setIsCurrentLiked] = useState(isLiked);
+
+  const { setModalView, openModal } = useUI();
+  const { isLogIn } = useAuthStore();
+
+  const likeMutation = useMutation({
+    mutationKey: [POST_KEY.LIKED_STATUS],
+    mutationFn: fetchPostLikedToggle,
+    onSuccess: ({ isLike }: LikesResponse) => {
+      setIsCurrentLiked(isLike);
+      router.refresh();
+    },
+    onError: ({ message }) => {
+      setModalView("ANIMATION_ALERT_VIEW");
+      openModal({
+        message: checkErrorCode({
+          errorCode: message,
+          defaultMessage: "해당 요청에 문제가 발생하였습니다.<br>잠시 후 다시 시도해주세요!",
+        }),
+      });
+    },
+  });
+
+  const handleLikeMutation = useCallback(
+    debounce((postId, isLike) => {
+      if (isCurrentLiked === isLike) {
+        return;
+      }
+
+      likeMutation.mutate({ postId: String(postId), data: { isLike: isLike } });
+    }, 1000),
+    [isCurrentLiked],
+  );
+
+  const handleClickLike = () => {
+    if (!isLogIn) {
+      setModalView("ACCESS_LOGIN_VIEW");
+      openModal();
+      return;
+    }
+
+    setLikes((likes) => (isLike ? likes - 1 : likes + 1));
+    setIsLike((prevIsLike) => {
+      const newIsLike = !prevIsLike;
+      handleLikeMutation(id, newIsLike); // state 업데이트 후 호출
+      return newIsLike;
+    });
+  };
+
+  return {
+    isLike,
+    likes,
+    handleClickLike,
+  };
+};
+
+export default useLikeMutation;
